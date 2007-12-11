17a98c144f44c519c1cf1192381033c10a973a37
okrasilni <okrasilni>
2007-12-11 16:55:21 +0000

    Bug # 182450: allow changing settings for multimple configs simultaneously

--- all/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/newui/CDTUIListComparator.java
+++ all/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/newui/CDTUIListComparator.java
@@ -14,49 +14,25 @@ import java.util.Comparator;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 
-import org.eclipse.cdt.core.cdtvariables.ICdtVariable;
-import org.eclipse.cdt.core.settings.model.ICConfigurationDescription;
-import org.eclipse.cdt.core.settings.model.ICLanguageSetting;
-import org.eclipse.cdt.core.settings.model.ICLanguageSettingEntry;
+import org.eclipse.cdt.core.model.util.CDTListComparator;
 import org.eclipse.cdt.ui.newui.AbstractExportTab.ExtData;
 import org.eclipse.cdt.ui.wizards.EntryDescriptor;
 
-public class CDTListComparator implements Comparator {
-	private static CDTListComparator comparator = null;
+public class CDTUIListComparator extends CDTListComparator implements Comparator {
 
-	public static CDTListComparator getInstance() {
+	public static Comparator getInstance() {
 		if (comparator == null)
-			comparator = new CDTListComparator();
+			comparator = new CDTUIListComparator();
 		return comparator;
 	}
 	public int compare(Object a, Object b) {
 		if (a == null || b == null) 
 			return 0;
-		if (a instanceof ICLanguageSetting) {
-			ICLanguageSetting c1 = (ICLanguageSetting)a;
-			ICLanguageSetting c2 = (ICLanguageSetting)b;
-			return c1.getName().compareToIgnoreCase(c2.getName());
-		} 
-		if (a instanceof ICLanguageSettingEntry) {
-			ICLanguageSettingEntry c1 = (ICLanguageSettingEntry)a;
-			ICLanguageSettingEntry c2 = (ICLanguageSettingEntry)b;
-			return c1.getName().compareToIgnoreCase(c2.getName());
-		} 
-		if (a instanceof ICConfigurationDescription) {
-			ICConfigurationDescription c1 = (ICConfigurationDescription)a;
-			ICConfigurationDescription c2 = (ICConfigurationDescription)b;
-			return c1.getName().compareToIgnoreCase(c2.getName());
-		}
 		if (a instanceof ExtData) {
 			ExtData c1 = (ExtData)a;
 			ExtData c2 = (ExtData)b;
 			return c1.getName().compareToIgnoreCase(c2.getName());
 		} 
-		if (a instanceof ICdtVariable) {
-			ICdtVariable c1 = (ICdtVariable) a;
-			ICdtVariable c2 = (ICdtVariable) b;
-			return c1.getName().compareToIgnoreCase(c2.getName());			
-		}
 		if (a instanceof IConfigurationElement) {
 			IConfigurationElement e1 = (IConfigurationElement)a;
 			IConfigurationElement e2 = (IConfigurationElement)b;
@@ -67,7 +43,7 @@ public class CDTListComparator implements Comparator {
 			EntryDescriptor c2 = (EntryDescriptor) b;
 			return c1.getName().compareToIgnoreCase(c2.getName());
 		}
-		return a.toString().compareTo(b.toString());
+		return super.compare(a, b);
 	}
 
 }

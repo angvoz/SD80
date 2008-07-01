d3e32d2e790e7bd2f4d58628effa3fe63817caf3
msennikovsky <msennikovsky>
2008-07-01 16:43:07 +0000

    Patch from Zhang, Yi Y, with small modification, for [Bug 229934] ManagedCommandLineGenerator.generateCommandLineInfo() skips text after last symbolic variable

--- all/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/core/ManagedCommandLineGenerator.java
+++ all/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/core/ManagedCommandLineGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 Intel Corporation and others.
+ * Copyright (c) 2004, 2007 Intel Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -131,6 +131,10 @@ public class ManagedCommandLineGenerator implements
 				start = ++stop;
 //			}
 		}
+		if (stop<commandLinePattern.length()){
+			sb.append(commandLinePattern.substring(stop));
+		}
+
 		return new ManagedCommandLineInfo( sb.toString().trim(), commandLinePattern, commandName, stringArrayToString( flags ),
 				outputFlag, outputPrefix, outputName, stringArrayToString( inputResources ) );
 	}

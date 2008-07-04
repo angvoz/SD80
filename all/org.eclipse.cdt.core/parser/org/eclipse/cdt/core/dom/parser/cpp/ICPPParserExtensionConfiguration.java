e08a354fa46db1e1d68a0f684b59e779c0706bba
mschorn <mschorn>
2008-07-04 09:22:25 +0000

    Simplify error-prone detection of end of template-args, bug 237045.

--- all/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/parser/cpp/ICPPParserExtensionConfiguration.java
+++ all/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/parser/cpp/ICPPParserExtensionConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2007 IBM Corporation and others.
+ * Copyright (c) 2002, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,8 +12,8 @@
  *******************************************************************************/
 package org.eclipse.cdt.core.dom.parser.cpp;
 
-import org.eclipse.cdt.core.dom.ast.IScope;
 import org.eclipse.cdt.core.dom.parser.IBuiltinBindingsProvider;
+import org.eclipse.cdt.core.dom.parser.IScannerExtensionConfiguration;
 
 /**
  * C++ parser extension configuration interface.
@@ -29,8 +29,8 @@ import org.eclipse.cdt.core.dom.parser.IBuiltinBindingsProvider;
  * with the CDT team.
  * </p>
  * 
- * @see http://gcc.gnu.org/onlinedocs/gcc/C-Extensions.html
- * @see http://gcc.gnu.org/onlinedocs/gcc/C_002b_002b-Extensions.html
+ * @see "http://gcc.gnu.org/onlinedocs/gcc/C-Extensions.html"
+ * @see "http://gcc.gnu.org/onlinedocs/gcc/C_002b_002b-Extensions.html"
  * 
  * @author jcamelon
  * @since 4.0
@@ -40,7 +40,7 @@ public interface ICPPParserExtensionConfiguration {
 	/**
 	 * Support for GNU extension "Restricting Pointer Aliasing".
 	 * 
-	 * @see http://gcc.gnu.org/onlinedocs/gcc/Restricted-Pointers.html
+	 * @see "http://gcc.gnu.org/onlinedocs/gcc/Restricted-Pointers.html"
 	 * @return <code>true</code> if support for the extension should be
 	 *         enabled
 	 */
@@ -49,45 +49,25 @@ public interface ICPPParserExtensionConfiguration {
 	/**
 	 * Support for GNU extension "Extended Syntax for Template Instantiation".
 	 * 
-	 * @see http://gcc.gnu.org/onlinedocs/gcc/Template-Instantiation.html
+	 * @see "http://gcc.gnu.org/onlinedocs/gcc/Template-Instantiation.html"
 	 * @return <code>true</code> if support for the extension should be
 	 *         enabled
 	 */
 	public boolean supportExtendedTemplateSyntax();
 
 	/**
-	 * Support for (deprecated) GNU minimum and maximum operators ((<code>&lt;?</code> and
-	 * <code>&gt;?</code>). If enabled, scanner extension support for those operators must
-	 * also be enabled.
-	 * 
-	 * @see IScannerExtensionConfiguration
-	 * @see http://gcc.gnu.org/onlinedocs/gcc/Deprecated-Features.html
-	 * @return <code>true</code> if support for the extension should be
-	 *         enabled
-	 */
-	public boolean supportMinAndMaxOperators();
-
-	/**
 	 * Support for GNU extension "Data types for complex numbers".
 	 * 
-	 * @see http://gcc.gnu.org/onlinedocs/gcc/Complex.html#Complex
+	 * @see "http://gcc.gnu.org/onlinedocs/gcc/Complex.html#Complex"
 	 * @return <code>true</code> if support for the extension should be
 	 *         enabled
 	 */
 	public boolean supportComplexNumbers();
 
 	/**
-	 * Support for the GNU <code>__restrict__</code> keyword.
-	 * 
-	 * @return <code>true</code> if support for the extension should be
-	 *         enabled
-	 */
-	public boolean supportRestrictKeyword();
-
-	/**
 	 * Support for GNU long long types.
 	 * 
-	 * @see http://gcc.gnu.org/onlinedocs/gcc/Long-Long.html
+	 * @see "http://gcc.gnu.org/onlinedocs/gcc/Long-Long.html"
 	 * @return <code>true</code> if support for the extension should be
 	 *         enabled
 	 */
@@ -96,7 +76,7 @@ public interface ICPPParserExtensionConfiguration {
 	/**
 	 * Support for GNU extension "Statements and Declarations in Expressions".
 	 * 
-	 * @see http://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html
+	 * @see "http://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html"
 	 * @return <code>true</code> if support for the extension should be
 	 *         enabled
 	 */
@@ -105,7 +85,7 @@ public interface ICPPParserExtensionConfiguration {
 	/**
 	 * Support for GNU extension "Referring to a Type with typeof".
 	 * 
-	 * @see http://gcc.gnu.org/onlinedocs/gcc/Typeof.html
+	 * @see "http://gcc.gnu.org/onlinedocs/gcc/Typeof.html"
 	 * @return <code>true</code> if support for the extension should be
 	 *         enabled
 	 */
@@ -114,7 +94,7 @@ public interface ICPPParserExtensionConfiguration {
 	/**
 	 * Support for GNU extension "Inquiring on Alignment of Types or Variables".
 	 * 
-	 * @see http://gcc.gnu.org/onlinedocs/gcc/Alignment.html
+	 * @see "http://gcc.gnu.org/onlinedocs/gcc/Alignment.html"
 	 * @return <code>true</code> if support for the extension should be
 	 *         enabled
 	 */
@@ -126,16 +106,6 @@ public interface ICPPParserExtensionConfiguration {
 	 * @return <code>true</code> if support for K&R C should be enabled
 	 */
 	public boolean supportKnRC();
-
-	/**
-	 * See http://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html for more
-	 * information on GCC's Other Built-in Symbols.
-	 * 
-	 * @return <code>true</code> if support for the extension should be
-	 *         enabled
-	 * @deprecated use {@link #getBuiltinSymbolProvider(IScope)} instead.
-	 */
-	public boolean supportGCCOtherBuiltinSymbols();
 
 	/**
 	 * See http://gcc.gnu.org/onlinedocs/gcc/Attribute-Syntax.html for more
@@ -161,4 +131,22 @@ public interface ICPPParserExtensionConfiguration {
 	 *         <code>null</code>
 	 */
 	public IBuiltinBindingsProvider getBuiltinBindingsProvider();
+	
+	/**
+	 * @deprecated use {@link #getBuiltinBindingsProvider()} instead.
+	 */
+	@Deprecated
+	public boolean supportGCCOtherBuiltinSymbols();
+	
+	/**
+	 * @deprecated use {@link IScannerExtensionConfiguration#supportMinAndMaxOperators()}, instead.     
+	 */
+	@Deprecated
+	public boolean supportMinAndMaxOperators();
+	
+	/**
+	 * @deprecated configure extra keywords, via {@link IScannerExtensionConfiguration#getAdditionalKeywords()}      
+	 */
+	@Deprecated
+	public boolean supportRestrictKeyword();
 }

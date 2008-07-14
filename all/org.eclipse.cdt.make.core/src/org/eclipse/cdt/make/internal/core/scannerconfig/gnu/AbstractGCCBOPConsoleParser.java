6d74fc0ce72d20d5b7826026b7b021929bb3abc4
mschorn <mschorn>
2008-07-14 16:00:53 +0000

    pwd in scanner discovery, bug 237958.

--- all/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/scannerconfig/gnu/AbstractGCCBOPConsoleParser.java
+++ all/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/scannerconfig/gnu/AbstractGCCBOPConsoleParser.java
@@ -1,14 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * IBM - Initial API and implementation
+ *     IBM - Initial API and implementation
+ *     Markus Schorn (Wind River Systems)
+ *     Gerhard Schaber (Wind River Systems) - bug 203059
  *******************************************************************************/
 package org.eclipse.cdt.make.internal.core.scannerconfig.gnu;
+
+import java.util.ArrayList;
 
 import org.eclipse.cdt.make.core.scannerconfig.IScannerInfoCollector;
 import org.eclipse.cdt.make.core.scannerconfig.IScannerInfoConsoleParser;
@@ -27,12 +31,17 @@ public abstract class AbstractGCCBOPConsoleParser implements IScannerInfoConsole
     private static final String[] COMPILER_INVOCATION = {
             "gcc", "g++", "cc", "c++" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
     };
+    protected static final String DASHIDASH= "-I-"; //$NON-NLS-1$
+    protected static final String DASHI= "-I"; //$NON-NLS-1$
+    protected static final String DASHD= "-D"; //$NON-NLS-1$
     
     private IProject project;
     private IScannerInfoCollector collector;
     
     private boolean bMultiline = false;
     private String sMultiline = ""; //$NON-NLS-1$
+
+	private String[] fCompilerCommands;
 
     /**
      * @return Returns the project.
@@ -50,6 +59,7 @@ public abstract class AbstractGCCBOPConsoleParser implements IScannerInfoConsole
     public void startup(IProject project, IScannerInfoCollector collector) {
         this.project = project;
         this.collector = collector;
+        fCompilerCommands= computeCompilerCommands();
     }
 
     /**
@@ -57,7 +67,7 @@ public abstract class AbstractGCCBOPConsoleParser implements IScannerInfoConsole
      * 
      * @return String[]
      */
-    public String[] getCompilerCommands() {
+    private String[] computeCompilerCommands() {
     	if (project != null) {
 	        SCProfileInstance profileInstance = ScannerConfigProfileManager.getInstance().
 	                getSCProfileInstance(project, ScannerConfigProfileManager.NULL_PROFILE_ID);
@@ -65,7 +75,7 @@ public abstract class AbstractGCCBOPConsoleParser implements IScannerInfoConsole
 	        if (boProvider != null) {
 	            String compilerCommandsString = boProvider.getScannerInfoConsoleParser().getCompilerCommands();
 	            if (compilerCommandsString != null && compilerCommandsString.length() > 0) {
-	                String[] compilerCommands = compilerCommandsString.split(",\\s+"); //$NON-NLS-1$
+	                String[] compilerCommands = compilerCommandsString.split(",\\s*"); //$NON-NLS-1$
 	                if (compilerCommands.length > 0) {
 	                    String[] compilerInvocation = new String[COMPILER_INVOCATION.length + compilerCommands.length];
 	                    System.arraycopy(COMPILER_INVOCATION, 0, compilerInvocation, 0, COMPILER_INVOCATION.length);
@@ -83,9 +93,20 @@ public abstract class AbstractGCCBOPConsoleParser implements IScannerInfoConsole
      */
     public boolean processLine(String line) {
         boolean rc = false;
+        int lineBreakPos = line.length()-1;
+        char[] lineChars = line.toCharArray();
+        while(lineBreakPos >= 0 && Character.isWhitespace(lineChars[lineBreakPos])) {
+        	lineBreakPos--;
+        }
+        if (lineBreakPos >= 0) {
+        	if (lineChars[lineBreakPos] != '\\'
+        	    || (lineBreakPos > 0 && lineChars[lineBreakPos-1] == '\\')) {
+        		lineBreakPos = -1;
+        	}
+        }
         // check for multiline commands (ends with '\')
-        if (line.endsWith("\\")) { //$NON-NLS-1$
-            sMultiline += line.substring(0, line.length()-1);// + " "; 
+        if (lineBreakPos >= 0) {
+       		sMultiline += line.substring(0, lineBreakPos);
             bMultiline = true;
             return rc;
         }
@@ -94,6 +115,7 @@ public abstract class AbstractGCCBOPConsoleParser implements IScannerInfoConsole
             bMultiline = false;
             sMultiline = ""; //$NON-NLS-1$
         }
+        line= line.trim();
         TraceUtil.outputTrace("AbstractGCCBOPConsoleParser parsing line: [", line, "]");    //$NON-NLS-1$ //$NON-NLS-2$
         // make\[[0-9]*\]:  error_desc
         int firstColon= line.indexOf(':');
@@ -115,7 +137,7 @@ public abstract class AbstractGCCBOPConsoleParser implements IScannerInfoConsole
             }
         }
         // call sublclass to process a single line
-        return processSingleLine(line);
+        return processSingleLine(line.trim());
     }
 
     private int getDirectoryLevel(String line) {
@@ -132,7 +154,6 @@ public abstract class AbstractGCCBOPConsoleParser implements IScannerInfoConsole
         return num;
     }
 
-    protected abstract boolean processSingleLine(String line);
     protected abstract AbstractGCCBOPConsoleParserUtility getUtility();
     
     /* (non-Javadoc)
@@ -143,4 +164,169 @@ public abstract class AbstractGCCBOPConsoleParser implements IScannerInfoConsole
             getUtility().reportProblems();
         }
     }
+    
+	/**
+	 * Tokenizes a line into an array of commands. Commands are separated by 
+	 * ';', '&&' or '||'. Tokens are separated by whitespace unless found inside
+	 * of quotes, back-quotes, or double quotes.
+	 * Outside of single-, double- or back-quotes a backslash escapes white-spaces, all quotes, 
+	 * the backslash, '&' and '|'.
+	 * A backslash used for escaping is removed.
+	 * Quotes other than the back-quote plus '&&', '||', ';' are removed, also.
+	 * @param line to tokenize
+	 * @return array of commands
+	 */
+	protected String[][] tokenize(String line, boolean escapeInsideDoubleQuotes) {
+		ArrayList<String[]> commands= new ArrayList<String[]>();
+		ArrayList<String> tokens= new ArrayList<String>();
+		StringBuffer token= new StringBuffer();
+		
+		final char[] input= line.toCharArray();
+		boolean nextEscaped= false;
+		char currentQuote= 0;
+		for (int i = 0; i < input.length; i++) {
+			final char c = input[i];
+			final boolean escaped= nextEscaped; nextEscaped= false;
+			
+			if (currentQuote != 0) {
+				if (c == currentQuote) {
+					if (escaped) {
+						token.append(c);
+					}
+					else {
+						if (c=='`') {
+							token.append(c);	// preserve back-quotes
+						}
+						currentQuote= 0;
+					}
+				}
+				else {
+					if (escapeInsideDoubleQuotes && currentQuote == '"' && c == '\\') {
+						nextEscaped= !escaped;
+						if (escaped) {
+							token.append(c);
+						}
+					}
+					else {
+						if (escaped) {
+							token.append('\\');
+						}
+						token.append(c);
+					}
+				}
+			}
+			else {
+				switch(c) {
+				case '\\':
+					if (escaped) {
+						token.append(c);
+					}
+					else {
+						nextEscaped= true;
+					}
+					break;
+				case '\'': case '"': case '`':
+					if (escaped) {
+						token.append(c);
+					}
+					else {
+						if (c == '`') {
+							token.append(c);
+						}
+						currentQuote= c;
+					}
+					break;
+				case ';':
+					if (escaped) {
+						token.append(c);
+					}
+					else {
+						endCommand(token, tokens, commands);
+					}
+					break;
+				case '&': case '|':
+					if (escaped || i+1 >= input.length || input[i+1] != c) {
+						token.append(c);
+					}
+					else {
+						i++;
+						endCommand(token, tokens, commands);
+					}
+					break;
+					
+				default:
+					if (Character.isWhitespace(c)) {
+						if (escaped) {
+							token.append(c);
+						}
+						else {
+							endToken(token, tokens);
+						}
+					}
+					else {
+						if (escaped) {
+							token.append('\\');	// for windows put backslash back onto the token.
+						}
+						token.append(c);
+					}
+				}
+			}
+		}
+		endCommand(token, tokens, commands);
+		return commands.toArray(new String[commands.size()][]);
+	}
+	
+	private void endCommand(StringBuffer token, ArrayList<String> tokens, ArrayList<String[]> commands) {
+		endToken(token, tokens);
+		if (!tokens.isEmpty()) {
+			commands.add(tokens.toArray(new String[tokens.size()]));
+			tokens.clear();
+		}
+	}
+	private void endToken(StringBuffer token, ArrayList<String> tokens) {
+		if (token.length() > 0) {
+			tokens.add(token.toString());
+			token.setLength(0);
+		}
+	}
+	
+    protected boolean processSingleLine(String line) {
+    	boolean rc= false;
+		String[][] tokens= tokenize(line, true);
+		for (int i = 0; i < tokens.length; i++) {
+			String[] command = tokens[i];
+			if (processCommand(command)) {
+				rc= true;
+			}
+			else {  // go inside quotes, if the compiler is called per wrapper or shell script
+				for (int j = 0; j < command.length; j++) {
+					String[][] subtokens= tokenize(command[j], true);
+					for (int k = 0; k < subtokens.length; k++) {
+						String[] subcommand = subtokens[k];
+						if (subcommand.length > 1) {  // only proceed if there is any additional info
+							if (processCommand(subcommand)) {
+								rc= true;
+							}
+						}
+					}
+				}
+			}
+		}
+		return rc;
+    }
+    
+    protected int findCompilerInvocation(String[] tokens) {
+    	for (int i = 0; i < tokens.length; i++) {
+			final String token = tokens[i].toLowerCase();
+    		final int searchFromOffset= Math.max(token.lastIndexOf('/'), token.lastIndexOf('\\')) + 1;
+    		for (int j=0; j < fCompilerCommands.length; j++) {
+    			if (token.indexOf(fCompilerCommands[j], searchFromOffset) != -1) {
+    				return i;
+    			}
+    		}
+    	}
+    	return -1;
+    }
+
+    abstract protected boolean processCommand(String[] command);
 }

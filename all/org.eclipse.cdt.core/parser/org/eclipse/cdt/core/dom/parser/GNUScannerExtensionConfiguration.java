213a211d32451d0a052f5eb10d658b3e03322a19
sprigogin <sprigogin>
2008-07-14 01:27:20 +0000

    Different definitions of __null for C and C++ (bug 240567).

--- all/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/parser/GNUScannerExtensionConfiguration.java
+++ all/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/parser/GNUScannerExtensionConfiguration.java
@@ -1,120 +1,88 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2007 IBM Corporation and others.
+ * Copyright (c) 2004, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * IBM - Initial API and implementation
- * Anton Leherbauer (Wind River Systems)
+ *    IBM - Initial API and implementation
+ *    Anton Leherbauer (Wind River Systems)
+ *    Markus Schorn (Wind River Systems)
  *******************************************************************************/
 package org.eclipse.cdt.core.dom.parser;
 
+import org.eclipse.cdt.core.parser.GCCKeywords;
+import org.eclipse.cdt.core.parser.IGCCToken;
+import org.eclipse.cdt.core.parser.IMacro;
 import org.eclipse.cdt.core.parser.IPreprocessorDirective;
+import org.eclipse.cdt.core.parser.IToken;
 import org.eclipse.cdt.core.parser.Keywords;
 import org.eclipse.cdt.core.parser.util.CharArrayIntMap;
-import org.eclipse.cdt.core.parser.util.CharArrayObjectMap;
-import org.eclipse.cdt.internal.core.parser.scanner2.FunctionStyleMacro;
-import org.eclipse.cdt.internal.core.parser.scanner2.ObjectStyleMacro;
 
 /**
  * @author jcamelon
  */
 public abstract class GNUScannerExtensionConfiguration extends AbstractScannerExtensionConfiguration {
 
-    protected static final char[] emptyCharArray = "".toCharArray(); //$NON-NLS-1$
+	private static IMacro[] sAdditionalMacros= new IMacro[] {
+		createMacro("__complex__", "_Complex"), //$NON-NLS-1$ //$NON-NLS-2$
+		createMacro("__extension__", ""), //$NON-NLS-1$ //$NON-NLS-2$
+		createMacro("__imag__", "(int)"), //$NON-NLS-1$ //$NON-NLS-2$
+		createMacro("__real__", "(int)"), //$NON-NLS-1$ //$NON-NLS-2$
+		createMacro("__stdcall", ""), //$NON-NLS-1$ //$NON-NLS-2$
 
-    public boolean initializeMacroValuesTo1() {
+		createMacro("__builtin_va_arg(ap,type)", "*(type *)ap"),  //$NON-NLS-1$//$NON-NLS-2$
+		createMacro("__builtin_constant_p(exp)", "0") //$NON-NLS-1$//$NON-NLS-2$
+	};
+
+	public static IMacro[] getAdditionalGNUMacros() {
+		return sAdditionalMacros;
+	}
+	
+	public static void addAdditionalGNUKeywords(CharArrayIntMap target) {
+		target.put(GCCKeywords.cp__ALIGNOF__, IGCCToken.t___alignof__ );
+		target.put(GCCKeywords.cp__ALIGNOF__, IGCCToken.t___alignof__ );
+		target.put(GCCKeywords.cp__ASM, IToken.t_asm); 
+		target.put(GCCKeywords.cp__ASM__, IToken.t_asm); 
+		target.put(GCCKeywords.cp__ATTRIBUTE, IGCCToken.t__attribute__ );
+		target.put(GCCKeywords.cp__ATTRIBUTE__, IGCCToken.t__attribute__ );
+		target.put(GCCKeywords.cp__CONST, IToken.t_const); 
+		target.put(GCCKeywords.cp__CONST__, IToken.t_const); 
+		target.put(GCCKeywords.cp__DECLSPEC, IGCCToken.t__declspec );
+		target.put(GCCKeywords.cp__INLINE, IToken.t_inline); 
+		target.put(GCCKeywords.cp__INLINE__, IToken.t_inline); 
+		target.put(GCCKeywords.cp__RESTRICT, IToken.t_restrict); 
+		target.put(GCCKeywords.cp__RESTRICT__, IToken.t_restrict); 
+		target.put(GCCKeywords.cp__VOLATILE, IToken.t_volatile); 
+		target.put(GCCKeywords.cp__VOLATILE__, IToken.t_volatile); 
+		target.put(GCCKeywords.cp__SIGNED, IToken.t_signed); 
+		target.put(GCCKeywords.cp__SIGNED__, IToken.t_signed); 
+		target.put(GCCKeywords.cp__TYPEOF, IGCCToken.t_typeof); 
+		target.put(GCCKeywords.cp__TYPEOF__, IGCCToken.t_typeof); 
+		target.put(GCCKeywords.cpTYPEOF, IGCCToken.t_typeof );
+	}
+
+    @Override
+	public boolean support$InIdentifiers() {
         return true;
     }
 
-    public boolean support$InIdentifiers() {
-        return true;
-    }
-
-    public char[] supportAdditionalNumericLiteralSuffixes() {
+    @Override
+	public char[] supportAdditionalNumericLiteralSuffixes() {
         return "ij".toCharArray(); //$NON-NLS-1$
     }
-    
-    private static final ObjectStyleMacro __asm__ = new ObjectStyleMacro(
-            "__asm__".toCharArray(), "asm".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-
-    private static final ObjectStyleMacro __inline__ = new ObjectStyleMacro(
-            "__inline__".toCharArray(), "inline".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-    private static final ObjectStyleMacro __extension__ = new ObjectStyleMacro(
-            "__extension__".toCharArray(), emptyCharArray); //$NON-NLS-1$
-
-
-    private static final ObjectStyleMacro __restrict__ = new ObjectStyleMacro(
-            "__restrict__".toCharArray(), "restrict".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-    private static final ObjectStyleMacro __restrict = new ObjectStyleMacro(
-            "__restrict".toCharArray(), "restrict".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-    private static final ObjectStyleMacro __volatile__ = new ObjectStyleMacro(
-            "__volatile__".toCharArray(), "volatile".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-    private static final ObjectStyleMacro __const__ = new ObjectStyleMacro(
-            "__const__".toCharArray(), "const".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-    private static final ObjectStyleMacro __const = new ObjectStyleMacro(
-            "__const".toCharArray(), "const".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-    private static final ObjectStyleMacro __signed__ = new ObjectStyleMacro(
-            "__signed__".toCharArray(), "signed".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-    private static final ObjectStyleMacro __complex__ = new ObjectStyleMacro(
-            "__complex__".toCharArray(), "_Complex".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-    private static final ObjectStyleMacro __real__ = new ObjectStyleMacro(
-            "__real__".toCharArray(), "(int)".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-    private static final ObjectStyleMacro __imag__ = new ObjectStyleMacro(
-            "__imag__".toCharArray(), "(int)".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-    
-    private static final ObjectStyleMacro __null = new ObjectStyleMacro(
-    		"__null".toCharArray(), "(void *)0".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
-    private static final FunctionStyleMacro __builtin_va_arg = new FunctionStyleMacro(
-            "__builtin_va_arg".toCharArray(), //$NON-NLS-1$
-            "*(type *)ap".toCharArray(), //$NON-NLS-1$
-            new char[][] { "ap".toCharArray(), "type".toCharArray() }); //$NON-NLS-1$//$NON-NLS-2$
-
-    private static final FunctionStyleMacro __builtin_constant_p = new FunctionStyleMacro(
-            "__builtin_constant_p".toCharArray(), //$NON-NLS-1$
-            "0".toCharArray(), //$NON-NLS-1$
-            new char[][] { "exp".toCharArray() }); //$NON-NLS-1$
-
-    /**
-     * @return
-     */
-    public CharArrayObjectMap getAdditionalMacros() {
-        CharArrayObjectMap realDefinitions = new CharArrayObjectMap(16);
-        realDefinitions.put(__inline__.name, __inline__);
-        realDefinitions.put(__const__.name, __const__);
-        realDefinitions.put(__const.name, __const);
-        realDefinitions.put(__extension__.name, __extension__);
-        realDefinitions.put(__restrict__.name, __restrict__);
-        realDefinitions.put(__restrict.name, __restrict);
-        realDefinitions.put(__volatile__.name, __volatile__);
-        realDefinitions.put(__signed__.name, __signed__);
-        realDefinitions.put(__complex__.name, __complex__);
-        realDefinitions.put(__imag__.name, __imag__);
-        realDefinitions.put( __null.name, __null );
-        realDefinitions.put(__real__.name, __real__);
-        realDefinitions.put(__builtin_va_arg.name, __builtin_va_arg);
-        realDefinitions.put(__builtin_constant_p.name, __builtin_constant_p);
-        realDefinitions.put( __asm__.name, __asm__ );
-        return realDefinitions;
+        
+    @Override
+	public IMacro[] getAdditionalMacros() {
+    	return sAdditionalMacros;
     }
     
     /*
      * @see org.eclipse.cdt.internal.core.parser.scanner2.IScannerExtensionConfiguration#getAdditionalPreprocessorKeywords()
      */
-    public CharArrayIntMap getAdditionalPreprocessorKeywords() {
+    @Override
+	public CharArrayIntMap getAdditionalPreprocessorKeywords() {
     	CharArrayIntMap additionalPPKeywords= new CharArrayIntMap(8, IPreprocessorDirective.ppInvalid);
         additionalPPKeywords.put(Keywords.cINCLUDE_NEXT, IPreprocessorDirective.ppInclude_next); 
         additionalPPKeywords.put(Keywords.cIMPORT, IPreprocessorDirective.ppImport);
--- all/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/parser/c/GCCScannerExtensionConfiguration.java
+++ all/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/parser/c/GCCScannerExtensionConfiguration.java
@@ -1,80 +1,61 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2007 IBM Corporation and others.
+ * Copyright (c) 2004, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * IBM - Initial API and implementation
- * Ed Swartz (Nokia)
+ *    IBM - Initial API and implementation
+ *    Ed Swartz (Nokia)
+ *    Markus Schorn (Wind River Systems)
+ *    Sergey Prigogin (Google)
  *******************************************************************************/
 package org.eclipse.cdt.core.dom.parser.c;
 
 import org.eclipse.cdt.core.dom.parser.GNUScannerExtensionConfiguration;
-import org.eclipse.cdt.core.parser.GCCKeywords;
-import org.eclipse.cdt.core.parser.IGCCToken;
+import org.eclipse.cdt.core.parser.IMacro;
 import org.eclipse.cdt.core.parser.util.CharArrayIntMap;
-import org.eclipse.cdt.core.parser.util.CharArrayObjectMap;
-import org.eclipse.cdt.internal.core.parser.scanner2.DynamicFunctionStyleMacro;
-import org.eclipse.cdt.internal.core.parser.scanner2.ExpressionEvaluator;
-import org.eclipse.cdt.internal.core.parser.scanner2.FunctionStyleMacro;
 
 /**
  * @author jcamelon
  */
 public class GCCScannerExtensionConfiguration extends GNUScannerExtensionConfiguration {
 
+	private static IMacro[] sAdditionalMacros;
+	private static CharArrayIntMap sAdditionalKeywords;
+	static {
+		final IMacro[] macros = GNUScannerExtensionConfiguration.getAdditionalGNUMacros();
+		sAdditionalMacros= new IMacro[macros.length + 2];
+		System.arraycopy(macros, 0, sAdditionalMacros, 0, macros.length);
+		sAdditionalMacros[macros.length]= createMacro("__null", "(void*)0");  //$NON-NLS-1$ //$NON-NLS-2$
+		sAdditionalMacros[macros.length + 1]= createMacro("_Pragma(arg)", "");  //$NON-NLS-1$//$NON-NLS-2$
+		
+		sAdditionalKeywords= new CharArrayIntMap(10, -1);
+		GNUScannerExtensionConfiguration.addAdditionalGNUKeywords(sAdditionalKeywords);
+	}
+
     /* (non-Javadoc)
      * @see org.eclipse.cdt.internal.core.parser.scanner2.IScannerConfiguration#supportMinAndMaxOperators()
      */
-    public boolean supportMinAndMaxOperators() {
+    @Override
+	public boolean supportMinAndMaxOperators() {
         return false;
     }
     
-	private static final FunctionStyleMacro _Pragma = new FunctionStyleMacro( 
-			"_Pragma".toCharArray(),  //$NON-NLS-1$
-			emptyCharArray, 
-			new char[][] { "arg".toCharArray() } ); //$NON-NLS-1$
-
-	private final DynamicFunctionStyleMacro __builtin_choose_expr = 
-		new DynamicFunctionStyleMacro( "__builtin_choose_expr".toCharArray(),  //$NON-NLS-1$
-		        					   new char[][] { "const_exp".toCharArray(), "exp1".toCharArray(), "exp2".toCharArray() } ) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{
-	    	public char [] execute( CharArrayObjectMap argmap ){
-                ExpressionEvaluator evaluator = new ExpressionEvaluator();
-                char[] const_exp = (char[]) argmap.get( arglist[0] );
-                long exp = 0;
-                if( const_exp != null)
-                    exp = evaluator.evaluate( const_exp, 0, const_exp.length, CharArrayObjectMap.EMPTY_MAP );
-                
-                if( exp != 0 )
-                    return (char[])argmap.get( arglist[1] );
-                return (char[])argmap.get( arglist[2] );
-            } 
-		};
-
-
     /* (non-Javadoc)
      * @see org.eclipse.cdt.internal.core.parser.scanner2.IScannerConfiguration#getAdditionalMacros()
      */
-    public CharArrayObjectMap getAdditionalMacros() {
-        CharArrayObjectMap result = super.getAdditionalMacros();
-        result.put(_Pragma.name, _Pragma );
-        result.put( __builtin_choose_expr.name, __builtin_choose_expr );
-        return result;
+    @Override
+	public IMacro[] getAdditionalMacros() {
+    	return sAdditionalMacros;
     }
 
     /* (non-Javadoc)
      * @see org.eclipse.cdt.internal.core.parser.scanner2.IScannerConfiguration#getAdditionalKeywords()
      */
-    public CharArrayIntMap getAdditionalKeywords() {
-        CharArrayIntMap result = new CharArrayIntMap( 4, -1 );
-		result.put( GCCKeywords.cp__ALIGNOF__, IGCCToken.t___alignof__ );
-		result.put( GCCKeywords.cpTYPEOF, IGCCToken.t_typeof );
-		result.put( GCCKeywords.cp__ATTRIBUTE__, IGCCToken.t__attribute__ );
-		result.put( GCCKeywords.cp__DECLSPEC, IGCCToken.t__declspec );
-        return result;
+    @Override
+	public CharArrayIntMap getAdditionalKeywords() {
+    	return sAdditionalKeywords;
     }
-
 }

5c1d9a64ce84923fc03f4f3307cb7e5b9277288f
crecoskie <crecoskie>
2008-08-15 15:53:49 +0000

    fixing accidental use of package visibility for makeArray()

--- all/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/core/MakeBuilder.java
+++ all/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/core/MakeBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 QNX Software Systems and others.
+ * Copyright (c) 2000, 2007 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -301,7 +301,7 @@ public class MakeBuilder extends ACBuilder {
 	}
 
 	// Turn the string into an array.
-	String[] makeArray(String string) {
+	private String[] makeArray(String string) {
 		string.trim();
 		char[] array = string.toCharArray();
 		ArrayList aList = new ArrayList();

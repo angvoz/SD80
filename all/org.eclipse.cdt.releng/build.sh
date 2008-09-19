09b4458af2a296c37e12456279be8337a91a700b
vkong <vkong>
2008-09-19 14:43:37 +0000

    save build log with the build

--- all/org.eclipse.cdt.releng/build.sh
+++ all/org.eclipse.cdt.releng/build.sh
@@ -1,5 +1,5 @@
 #*******************************************************************************
-# Copyright (c) 2005, 2006 IBM Corporation and others.
+# Copyright (c) 2005, 2008 IBM Corporation and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
@@ -8,15 +8,32 @@
 # Contributors:
 #     IBM Corporation - initial API and implementation
 #*******************************************************************************
-umask 0022
 
+# The CDT build script, set up the environment to run the build.xml ant script
+# We are running on build.eclipse.org
+
+# export display for running the tests
+export DISPLAY=:1
+
+# set up to use the Java 5 JRE
+export PATH=/opt/public/common/ibm-java2-ppc-50/bin:$PATH
+
+# make sure we're in the releng project dir 
 cd `dirname $0`
 
+# (TODO why is this here?)
+umask 0022
+
+# Checkout basebuilder to run the build
 mkdir -p tools
 cd tools
 cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse \
-	checkout org.eclipse.releng.basebuilder
+	checkout -r RC2_34 org.eclipse.releng.basebuilder
 cd ..
 
+# Let's go!
 java -jar tools/org.eclipse.releng.basebuilder/plugins/org.eclipse.equinox.launcher.jar \
-	-ws gtk -application org.eclipse.ant.core.antRunner $*
+	-ws gtk -arch ppc -os linux -application org.eclipse.ant.core.antRunner $*
+	
+# Copy the build.log to where the latest build is
+cp build.log /home/www/tools/cdt/builds/5.0.2/latest

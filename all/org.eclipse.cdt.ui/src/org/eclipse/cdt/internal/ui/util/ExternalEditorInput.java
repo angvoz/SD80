1031f109e92bc7084d0920d0a6562ff175b8dd8b
aleherbau <aleherbau>
2008-10-07 07:23:33 +0000

    Bug 245861 - [semantic highlighting] parser errors during editing leads to incorrect highlighting

--- all/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/util/ExternalEditorInput.java
+++ all/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/util/ExternalEditorInput.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 QNX Software Systems and others.
+ * Copyright (c) 2000, 2008 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,26 +12,32 @@
 package org.eclipse.cdt.internal.ui.util;
 
 
-import org.eclipse.jface.resource.ImageDescriptor;
+import java.net.URI;
 
-import org.eclipse.cdt.core.model.ITranslationUnit;
-import org.eclipse.cdt.internal.ui.editor.ITranslationUnitEditorInput;
-
+import org.eclipse.core.filesystem.URIUtil;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
-
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IEditorRegistry;
+import org.eclipse.ui.IMemento;
 import org.eclipse.ui.IPersistableElement;
+import org.eclipse.ui.IStorageEditorInput;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.editors.text.ILocationProvider;
+
+import org.eclipse.cdt.core.model.ITranslationUnit;
+import org.eclipse.cdt.core.resources.EFSFileStorage;
+
+import org.eclipse.cdt.internal.ui.editor.ITranslationUnitEditorInput;
 
 
 /**
  * An EditorInput for an external (non-workspace) file.
  */
-public class ExternalEditorInput implements ITranslationUnitEditorInput {
+public class ExternalEditorInput implements ITranslationUnitEditorInput, IPersistableElement {
            
 	private IStorage externalFile;
 	private IResource markerResource;
@@ -40,34 +46,35 @@ public class ExternalEditorInput implements ITranslationUnitEditorInput {
 
 	/*
 	*/
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (!(obj instanceof ExternalEditorInput))
+		if (!(obj instanceof IStorageEditorInput))
 			return false;
-		ExternalEditorInput other = (ExternalEditorInput)obj;
-		return externalFile.equals(other.externalFile);
+		IStorageEditorInput other = (IStorageEditorInput)obj;
+		try {
+			return externalFile.equals(other.getStorage());
+		} catch (CoreException exc) {
+			return false;
+		}
 	}
 
 	/*
 	* @see IEditorInput#exists()
 	*/
 	public boolean exists() {
-		// External file ca not be deleted
+		// External file can not be deleted
 		return true;
 	}
 
 	/*
 	* @see IAdaptable#getAdapter(Class)
 	*/
+	@SuppressWarnings("unchecked")
 	public Object getAdapter(Class adapter) {
 		if (ILocationProvider.class.equals(adapter)) {
-			// workaround for https://bugs.eclipse.org/bugs/show_bug.cgi?id=180003
-			// see also https://bugs.eclipse.org/bugs/show_bug.cgi?id=179982
-			if (location != null) {
-				return this;
-			}
-			return null;
+			return this;
 		}
 		return Platform.getAdapterManager().getAdapter(this, adapter);
 	}
@@ -91,6 +98,9 @@ public class ExternalEditorInput implements ITranslationUnitEditorInput {
 	* @see IEditorInput#getPersistable()
 	*/
 	public IPersistableElement getPersistable() {
+		if (location != null) {
+			return this;
+		}
 		return null;
 	}
 
@@ -119,7 +129,7 @@ public class ExternalEditorInput implements ITranslationUnitEditorInput {
 	 * @see org.eclipse.ui.editors.text.ILocationProvider#getPath(java.lang.Object)
 	 */
 	public IPath getPath(Object element) {
-		return location ;
+		return location;
 	}
 
 	public ExternalEditorInput(ITranslationUnit unit, IStorage exFile) {
@@ -152,7 +162,33 @@ public class ExternalEditorInput implements ITranslationUnitEditorInput {
 	public IResource getMarkerResource() {
 		return markerResource;
 	}
-	
-	
 
+	/*
+	 * @see org.eclipse.ui.IPersistableElement#getFactoryId()
+	 */
+	public String getFactoryId() {
+		return ExternalEditorInputFactory.ID;
+	}
+
+	/*
+	 * @see org.eclipse.ui.IPersistable#saveState(org.eclipse.ui.IMemento)
+	 */
+	public void saveState(IMemento memento) {
+		ExternalEditorInputFactory.saveState(memento, this);
+	}
+
+	/*
+	 * @see org.eclipse.ui.editors.text.ILocationProviderExtension#getURI(java.lang.Object)
+	 */
+	public URI getURI(Object element) {
+		if (externalFile instanceof EFSFileStorage) {
+			return ((EFSFileStorage) externalFile).getLocationURI();
+		}
+		IPath location = getPath(element);
+		if (location != null) {
+			return URIUtil.toURI(location);
+		}
+		return null;
+	}
+	
 }

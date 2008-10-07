442834cbf819af0215b2c326cdaa5bc07d6be352
aleherbau <aleherbau>
2008-10-07 07:23:32 +0000

    Bug 245861 - [semantic highlighting] parser errors during editing leads to incorrect highlighting

--- all/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/BasicCEditorTest.java
+++ all/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/BasicCEditorTest.java
@@ -16,11 +16,15 @@ import java.io.File;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.URIUtil;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IDocumentListener;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.text.source.SourceViewer;
@@ -30,7 +34,10 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 
 import org.eclipse.cdt.core.CCorePlugin;
 import org.eclipse.cdt.core.model.CModelException;
@@ -55,6 +62,17 @@ import org.eclipse.cdt.internal.ui.util.EditorUtility;
  */
 public class BasicCEditorTest extends BaseUITestCase {
 
+	final static class TestDocListener implements IDocumentListener {
+		public boolean fDocChanged;
+
+		public void documentAboutToBeChanged(DocumentEvent event) {
+		}
+
+		public void documentChanged(DocumentEvent event) {
+			fDocChanged= true;
+		}
+	}
+
 	private static CEditor fEditor;
 	private static SourceViewer fSourceViewer;
 	private ICProject fCProject;
@@ -62,15 +80,18 @@ public class BasicCEditorTest extends BaseUITestCase {
 	private StyledText fTextWidget;
 	private Accessor fAccessor;
 	private IDocument fDocument;
+	private TestDocListener fDocListener= new TestDocListener();
 
 	public static Test suite() {
-		return new TestSuite(BasicCEditorTest.class); 
+		return new TestSuite(BasicCEditorTest.class);
 	}
 	
+	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
 
+	@Override
 	protected void tearDown () throws Exception {
 		EditorTestHelper.closeEditor(fEditor);
 		if (fCProject != null)
@@ -81,8 +102,8 @@ public class BasicCEditorTest extends BaseUITestCase {
 		super.tearDown();
 	}
 
-	private void setUpEditor(String file) throws PartInitException {
-		IEditorPart editor= EditorTestHelper.openInEditor(ResourceTestHelper.findFile(file), true);
+	private void setUpEditor(IFile file) throws PartInitException {
+		IEditorPart editor= EditorTestHelper.openInEditor(file, true);
 		assertNotNull(editor);
 		assertTrue(editor instanceof CEditor);
 		fEditor= (CEditor) editor;
@@ -91,10 +112,14 @@ public class BasicCEditorTest extends BaseUITestCase {
 		fAccessor= new Accessor(fTextWidget, StyledText.class);
 		fDocument= fEditor.getDocumentProvider().getDocument(fEditor.getEditorInput());
 		assertNotNull(fDocument);
+	}
+
+	private void setUpEditor(String file) throws PartInitException {
+		setUpEditor(ResourceTestHelper.findFile(file));
 	}
 
 	private void setUpEditor(File file) throws PartInitException, CModelException {
-		IEditorPart editor= EditorUtility.openInEditor(new ExternalTranslationUnit(fCProject, Path.fromOSString(file.toString()), CCorePlugin.CONTENT_TYPE_CXXSOURCE));
+		IEditorPart editor= EditorUtility.openInEditor(new ExternalTranslationUnit(fCProject, file.toURI(), CCorePlugin.CONTENT_TYPE_CXXSOURCE));
 		assertNotNull(editor);
 		assertTrue(editor instanceof CEditor);
 		fEditor= (CEditor) editor;
@@ -105,6 +130,133 @@ public class BasicCEditorTest extends BaseUITestCase {
 		assertNotNull(fDocument);
 	}
 
+	private void setUpEditorUsingFileStore(File file) throws CoreException {
+		final IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		IEditorPart editor= IDE.openEditorOnFileStore(activePage, EFS.getStore(URIUtil.toURI(file.getAbsolutePath())));
+		assertNotNull(editor);
+		assertTrue(editor instanceof CEditor);
+		fEditor= (CEditor) editor;
+		fTextWidget= fEditor.getViewer().getTextWidget();
+		assertNotNull(fTextWidget);
+		fAccessor= new Accessor(fTextWidget, StyledText.class);
+		fDocument= fEditor.getDocumentProvider().getDocument(fEditor.getEditorInput());
+		assertNotNull(fDocument);
+	}
+
+	public void testEditTranslationUnit() throws Exception {
+		final String file= "/ceditor/src/main.cpp";
+		fCProject= EditorTestHelper.createCProject("ceditor", "resources/ceditor", false, false);
+		IFile mainFile= ResourceTestHelper.findFile(file);
+		assertNotNull(mainFile);
+		setUpEditor(mainFile);
+		fSourceViewer= EditorTestHelper.getSourceViewer(fEditor);
+		assertTrue(EditorTestHelper.joinReconciler(fSourceViewer, 0, 10000, 100));
+		String content= fDocument.get();
+		setCaret(0);
+		String newtext= "/* "+getName()+" */";
+		type(newtext);
+		type('\n');
+		String newContent= fDocument.get();
+		assertEquals("Edit failed", newtext, newContent.substring(0, newtext.length()));
+		// save
+		fEditor.doSave(new NullProgressMonitor());
+		assertFalse("Editor is still dirty", fEditor.isDirty());
+		// close and reopen
+		EditorTestHelper.closeEditor(fEditor);
+		setUpEditor(mainFile);
+		fSourceViewer= EditorTestHelper.getSourceViewer(fEditor);
+		assertTrue(EditorTestHelper.joinReconciler(fSourceViewer, 0, 10000, 100));
+		content= fDocument.get();
+		assertEquals("Save failed", newContent, content);
+		// check reconciler
+		ITranslationUnit tUnit= (ITranslationUnit)fEditor.getInputCElement();
+		ICElement[] children= tUnit.getChildren();
+		assertEquals(2, children.length);
+		setCaret(content.length());
+		type('\n');
+		type("void func() {}\n");
+		assertTrue(EditorTestHelper.joinReconciler(fSourceViewer, 0, 10000, 100));
+		children= tUnit.getChildren();
+		assertEquals(3, children.length);
+	}
+	//{Point.cpp}
+	//#include <math.h>
+	//class Point {
+	//public:
+	//	Point(double xc, double yc) :
+	//		x(xc), y(yc) {}
+	//	double distance(const Point& other) const;
+	//	int compareX(const Point& other) const;
+	//	double x;
+	//	double y;
+	//};
+	//double Point::distance(const Point& other) const {
+	//	double dx = x - other.x;
+	//	double dy = y - other.y;
+	//	return sqrt(dx * dx + dy * dy);
+	//}
+	//int Point::compareX(const Point& other) const {
+	//	if (x < other.x) {
+	//		return -1;
+	//	}
+	//	else if (x > other.x) {
+	//		return 1;
+	//	}
+	//	else {
+	//		return 0;
+	//	}
+	//}
+	//
+	public void testEditNewTranslationUnit() throws Exception {
+		fCProject= EditorTestHelper.createCProject("ceditor", "resources/ceditor", false, false);
+		IFile newFile= createFile(fCProject.getProject(), "Point.cpp", "");
+		assertNotNull(newFile);
+		setUpEditor(newFile);
+		fSourceViewer= EditorTestHelper.getSourceViewer(fEditor);
+		assertTrue(EditorTestHelper.joinReconciler(fSourceViewer, 0, 10000, 100));
+		String content= fDocument.get();
+		setCaret(0);
+		String newText= "/* "+getName()+" */\n";
+		newText += readTaggedComment("Point.cpp");
+		String[] lines= newText.split("\\r\\n|\\r|\\n");
+		for (int i = 0; i < lines.length; i++) {
+			String line= lines[i].trim();
+			if (line.startsWith("}")) {
+				setCaret(fDocument.get().indexOf(line, getCaret())+line.length());
+				Thread.sleep(100);
+			} else {
+				if (i > 0) type('\n');
+				type(line);
+				Thread.sleep(50);
+			}
+		}
+		String newContent= fDocument.get();
+		String[] newLines= newContent.split("\\r\\n|\\r|\\n");
+		for (int i = 0; i < lines.length; i++) {
+			String line= lines[i];
+			assertEquals(line, newLines[i]);
+		}
+		// save
+		fEditor.doSave(new NullProgressMonitor());
+		assertFalse("Editor is still dirty", fEditor.isDirty());
+		// close and reopen
+		EditorTestHelper.closeEditor(fEditor);
+		setUpEditor(newFile);
+		fSourceViewer= EditorTestHelper.getSourceViewer(fEditor);
+		assertTrue(EditorTestHelper.joinReconciler(fSourceViewer, 0, 10000, 100));
+		content= fDocument.get().trim();
+		assertEquals("Save failed", newContent, content);
+		// check reconciler
+		ITranslationUnit tUnit= (ITranslationUnit)fEditor.getInputCElement();
+		ICElement[] children= tUnit.getChildren();
+		assertEquals(4, children.length);
+		setCaret(content.length());
+		type('\n');
+		type("void func() {}\n");
+		assertTrue(EditorTestHelper.joinReconciler(fSourceViewer, 0, 10000, 100));
+		children= tUnit.getChildren();
+		assertEquals(5, children.length);
+	}
 	public void testEditInNonCProject() throws Exception {
 		final String file= "/ceditor/src/main.cpp";
 		fNonCProject= EditorTestHelper.createNonCProject("ceditor", "resources/ceditor", false);
@@ -130,7 +282,7 @@ public class BasicCEditorTest extends BaseUITestCase {
 
 	public void testEditExternalTranslationUnit() throws Exception {
 		final String file= "/ceditor/src/main.cpp";
-		fCProject= EditorTestHelper.createCProject("ceditor", "resources/ceditor", false);
+		fCProject= EditorTestHelper.createCProject("ceditor", "resources/ceditor", false, false);
 		IFile mainFile= ResourceTestHelper.findFile(file);
 		assertNotNull(mainFile);
 		File tmpFile= File.createTempFile("tmp", ".cpp");
@@ -169,13 +321,54 @@ public class BasicCEditorTest extends BaseUITestCase {
 		tmpFile.delete();
 	}
 
+	public void testEditExternalTranslationUnitUsingFileStore() throws Exception {
+		final String file= "/ceditor/src/main.cpp";
+		fCProject= EditorTestHelper.createCProject("ceditor", "resources/ceditor", false, false);
+		IFile mainFile= ResourceTestHelper.findFile(file);
+		assertNotNull(mainFile);
+		File tmpFile= File.createTempFile("tmp", ".cpp");
+		tmpFile.deleteOnExit();
+		FileTool.copy(mainFile.getLocation().toFile(), tmpFile);
+		setUpEditorUsingFileStore(tmpFile);
+		fSourceViewer= EditorTestHelper.getSourceViewer(fEditor);
+		assertTrue(EditorTestHelper.joinReconciler(fSourceViewer, 0, 10000, 100));
+		String content= fDocument.get();
+		setCaret(0);
+		String newtext= "/* "+getName()+" */";
+		type(newtext);
+		type('\n');
+		String newContent= fDocument.get();
+		assertEquals("Edit failed", newtext, newContent.substring(0, newtext.length()));
+		// save
+		fEditor.doSave(new NullProgressMonitor());
+		assertFalse("Editor is still dirty", fEditor.isDirty());
+		// close and reopen
+		EditorTestHelper.closeEditor(fEditor);
+		setUpEditor(tmpFile);
+		fSourceViewer= EditorTestHelper.getSourceViewer(fEditor);
+		assertTrue(EditorTestHelper.joinReconciler(fSourceViewer, 0, 10000, 100));
+		content= fDocument.get();
+		assertEquals("Save failed", newContent, content);
+		// check reconciler
+		ITranslationUnit tUnit= (ITranslationUnit)fEditor.getInputCElement();
+		ICElement[] children= tUnit.getChildren();
+		assertEquals(2, children.length);
+		setCaret(content.length());
+		type('\n');
+		type("void func() {}\n");
+		assertTrue(EditorTestHelper.joinReconciler(fSourceViewer, 0, 10000, 100));
+		children= tUnit.getChildren();
+		assertEquals(3, children.length);
+		tmpFile.delete();
+	}
+
 	public void testSyntaxHighlighting_Bug180433() throws Exception {
 		CColorManager colorMgr= CUIPlugin.getDefault().getTextTools().getColorManager();
 		colorMgr.unbindColor(ICColorConstants.PP_DIRECTIVE);
 		colorMgr.bindColor(ICColorConstants.PP_DIRECTIVE, new RGB(7,7,7));
 		final Color ppDirectiveColor= colorMgr.getColor(ICColorConstants.PP_DIRECTIVE);
 		final String file= "/ceditor/src/main.cpp";
-		fCProject= EditorTestHelper.createCProject("ceditor", "resources/ceditor", false);
+		fCProject= EditorTestHelper.createCProject("ceditor", "resources/ceditor", false, false);
 		setUpEditor(file);
 		fSourceViewer= EditorTestHelper.getSourceViewer(fEditor);
 		assertTrue(EditorTestHelper.joinReconciler(fSourceViewer, 0, 10000, 100));
@@ -213,6 +406,8 @@ public class BasicCEditorTest extends BaseUITestCase {
 	 * @param stateMask the state mask
 	 */
 	private void type(char character, int keyCode, int stateMask) {
+		fDocument.addDocumentListener(fDocListener);
+		fDocListener.fDocChanged= false;
 		Event event= new Event();
 		event.character= character;
 		event.keyCode= keyCode;
@@ -220,10 +415,12 @@ public class BasicCEditorTest extends BaseUITestCase {
 		fAccessor.invoke("handleKeyDown", new Object[] {event});
 		
 		new DisplayHelper() {
+			@Override
 			protected boolean condition() {
-				return false;
+				return fDocListener.fDocChanged;
 			}
 		}.waitForCondition(EditorTestHelper.getActiveDisplay(), 50);
+		fDocument.removeDocumentListener(fDocListener);
 	}
 
 	private int getCaret() {

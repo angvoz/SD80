b4160e3b8918ef2defefe68014c16e0bfe45f64b
mschorn <mschorn>
2008-11-03 10:20:10 +0000

    Fixes a CCE in CPPSpecialization, bug 253050.

--- all/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/CPPSpecialization.java
+++ all/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/CPPSpecialization.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  * IBM - Initial API and implementation
  * Markus Schorn (Wind River Systems)
- * /
+ *
  *******************************************************************************/
 /*
  * Created on Apr 29, 2005
@@ -20,48 +20,52 @@ import org.eclipse.cdt.core.dom.ast.DOMException;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
 import org.eclipse.cdt.core.dom.ast.IBinding;
 import org.eclipse.cdt.core.dom.ast.IScope;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPBinding;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPScope;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPSpecialization;
 import org.eclipse.cdt.core.parser.util.ArrayUtil;
 import org.eclipse.cdt.core.parser.util.ObjectMap;
 import org.eclipse.cdt.internal.core.dom.Linkage;
 import org.eclipse.cdt.internal.core.dom.parser.ASTNode;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.semantics.CPPVisitor;
 import org.eclipse.core.runtime.PlatformObject;
 
 /**
  * @author aniefer
- *
  */
-public abstract class CPPSpecialization extends PlatformObject implements ICPPSpecialization, ICPPInternalBinding {
+public abstract class CPPSpecialization extends PlatformObject
+		implements ICPPSpecialization, ICPPInternalBinding {
 	private IBinding specialized;
 	private ICPPScope scope;
 	protected ObjectMap argumentMap;
-	
-	private IASTNode definition = null;
-	private IASTNode [] declarations = null;
-	
-	public CPPSpecialization( IBinding specialized, ICPPScope scope, ObjectMap argumentMap ){
+
+	private IASTNode definition;
+	private IASTNode[] declarations;
+
+	public CPPSpecialization(IBinding specialized, ICPPScope scope, ObjectMap argumentMap) {
 		this.specialized = specialized;
 		this.scope = scope;
 		this.argumentMap = argumentMap;
-		
-		if( specialized instanceof ICPPInternalBinding ){
-			definition = ((ICPPInternalBinding)specialized).getDefinition();
-			IASTNode [] decls = ((ICPPInternalBinding)specialized).getDeclarations();
-			if( decls != null && decls.length > 0 )
-				declarations = new IASTNode[]{ decls[0] };
+
+		if (specialized instanceof ICPPInternalBinding) {
+			definition = ((ICPPInternalBinding) specialized).getDefinition();
+			IASTNode[] decls = ((ICPPInternalBinding) specialized).getDeclarations();
+			if (decls != null && decls.length > 0)
+				declarations = new IASTNode[] { decls[0] };
 		}
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.cdt.core.dom.ast.cpp.ICPPSpecialization#getSpecializedBinding()
 	 */
 	public IBinding getSpecializedBinding() {
 		return specialized;
 	}
+
 	public IASTNode[] getDeclarations() {
 		return declarations;
 	}
+
 	public IASTNode getDefinition() {
 		return definition;
 	}
@@ -69,49 +73,70 @@ public abstract class CPPSpecialization extends PlatformObject implements ICPPSp
 	public void addDefinition(IASTNode node) {
 		definition = node;
 	}
+
 	public void addDeclaration(IASTNode node) {
-		if( declarations == null )
+		if (declarations == null) {
 	        declarations = new IASTNode[] { node };
-	    else {
-	        //keep the lowest offset declaration in [0]
-			if( declarations.length > 0 && ((ASTNode)node).getOffset() < ((ASTNode)declarations[0]).getOffset() ){
-				declarations = (IASTNode[]) ArrayUtil.prepend( IASTNode.class, declarations, node );
+		} else {
+	        // keep the lowest offset declaration in [0]
+			if (declarations.length > 0 &&
+					((ASTNode) node).getOffset() < ((ASTNode) declarations[0]).getOffset()) {
+				declarations = (IASTNode[]) ArrayUtil.prepend(IASTNode.class, declarations, node);
 			} else {
-				declarations = (IASTNode[]) ArrayUtil.append( IASTNode.class, declarations, node );
+				declarations = (IASTNode[]) ArrayUtil.append(IASTNode.class, declarations, node);
 			}
 	    }
 	}
-	
+
 	public void removeDeclaration(IASTNode node) {
-		if( node == definition ){
+		if (node == definition) {
 			definition = null;
 			return;
 		}
 		ArrayUtil.remove(declarations, node);
 	}
-	
+
 	public String getName() {
 		return specialized.getName();
 	}
+
 	public char[] getNameCharArray() {
 		return specialized.getNameCharArray();
 	}
+
 	public IScope getScope() {
 		return scope;
 	}
+
 	public String[] getQualifiedName() {
-		return CPPVisitor.getQualifiedName( this );
+		return CPPVisitor.getQualifiedName(this);
 	}
+
 	public char[][] getQualifiedNameCharArray() {
-		return CPPVisitor.getQualifiedNameCharArray( this );
+		return CPPVisitor.getQualifiedNameCharArray(this);
 	}
+
 	public boolean isGloballyQualified() throws DOMException {
-		return ((ICPPInternalBinding)specialized).isGloballyQualified();
+		if (specialized instanceof ICPPBinding)
+			return ((ICPPBinding) specialized).isGloballyQualified();
+		return false;
 	}
+
 	public ILinkage getLinkage() {
 		return Linkage.CPP_LINKAGE;
 	}
+
 	public ObjectMap getArgumentMap() {
 		return argumentMap;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder result = new StringBuilder(getName());
+		if (argumentMap != null) {
+			result.append(" "); //$NON-NLS-1$
+			result.append(argumentMap.toString());
+		}
+		return result.toString();
 	}
 }

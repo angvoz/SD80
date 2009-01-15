9f0d87bc02de344cc8d1777925c0f8013aa11eeb
elaskavaia <elaskavaia>
2009-01-15 19:09:56 +0000

    [240616] make target should not invoke extra clean command, applied patch

--- all/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/MakeTarget.java
+++ all/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/MakeTarget.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 QNX Software Systems and others.
+ * Copyright (c) 2000, 2007 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -290,9 +290,10 @@ public class MakeTarget extends PlatformObject implements IMakeTarget {
 		info.setStopOnError(isStopOnError());
 		info.setIncrementalBuildEnable(true);
 		info.setBuildAttribute(IMakeBuilderInfo.BUILD_TARGET_INCREMENTAL, getBuildAttribute(IMakeTarget.BUILD_TARGET, "")); //$NON-NLS-1$
+		info.setCleanBuildEnable(false);
 		info.setEnvironment(getExpandedEnvironment());
 		info.setAppendEnvironment(appendEnvironment());
-		if (container != null) {
+		if (container != null && container != project) {
 			info.setBuildAttribute(IMakeCommonBuildInfo.BUILD_LOCATION, container.getFullPath().toString());
 		}
 		IMakeBuilderInfo projectInfo = MakeCorePlugin.createBuildInfo(getProject(), builderID);

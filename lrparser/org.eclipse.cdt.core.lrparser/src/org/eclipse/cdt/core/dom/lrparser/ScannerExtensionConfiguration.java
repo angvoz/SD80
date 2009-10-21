158527c43e36ba66e9dfb3c71e9590279b03ec69
mkucera <mkucera>
2009-10-21 17:17:53 +0000

    Bug 292782 -  backport enhancements to LR parser into 5.0 branch

---
+++ lrparser/org.eclipse.cdt.core.lrparser/src/org/eclipse/cdt/core/dom/lrparser/ScannerExtensionConfiguration.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.cdt.core.dom.lrparser;
+
+import org.eclipse.cdt.core.dom.parser.AbstractScannerExtensionConfiguration;
+import org.eclipse.cdt.core.parser.IMacro;
+
+
+/**
+ * A minimalistic scanner configuration for the LR parser.
+ * 
+ * @author Mike Kucera
+ *
+ */
+@SuppressWarnings("nls")
+public class ScannerExtensionConfiguration extends AbstractScannerExtensionConfiguration {
+
+	
+	private ScannerExtensionConfiguration() {
+	}
+	
+	public static ScannerExtensionConfiguration createC() {
+		return new ScannerExtensionConfiguration() {
+			@Override public IMacro[] getAdditionalMacros() {
+				return new IMacro[]	{ createMacro("__null", "(void *)0") };
+			}
+		};
+	}
+	
+	public static ScannerExtensionConfiguration createCPP() {
+		return new ScannerExtensionConfiguration() {
+			@Override public IMacro[] getAdditionalMacros() {
+				return new IMacro[]	{ createMacro("__null", "0") };
+			}
+		};
+	}
+
+	@Override
+	public boolean support$InIdentifiers() {
+		return true;
+	}
+
+}

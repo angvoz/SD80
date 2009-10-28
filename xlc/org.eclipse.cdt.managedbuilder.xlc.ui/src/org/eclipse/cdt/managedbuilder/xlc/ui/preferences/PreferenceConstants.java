ca7baff28253912f570c0ca4031c7f580f16ea40
crecoskie <crecoskie>
2009-10-28 16:54:35 +0000

    remote scanner config support

---
+++ xlc/org.eclipse.cdt.managedbuilder.xlc.ui/src/org/eclipse/cdt/managedbuilder/xlc/ui/preferences/PreferenceConstants.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.cdt.managedbuilder.xlc.ui.preferences;
+
+import org.eclipse.cdt.managedbuilder.xlc.ui.Messages;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_XL_COMPILER_ROOT = "XL_compilerRoot"; //$NON-NLS-1$
+
+	public static final String P_XLC_COMPILER_VERSION = "XLC_compilerVersion";  //$NON-NLS-1$
+	
+	public static final String P_XL_COMPILER_VERSION_8_NAME = Messages.XLCompiler_v8;
+	public static final String P_XL_COMPILER_VERSION_9_NAME = Messages.XLCompiler_v9;
+	public static final String P_XL_COMPILER_VERSION_10_NAME = Messages.XLCompiler_v10;
+
+}

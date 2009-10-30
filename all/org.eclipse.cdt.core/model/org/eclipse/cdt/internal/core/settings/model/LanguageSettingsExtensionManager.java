43c57e3e295f1cbc07d5c510ae907e458feea440
agvozdev <agvozdev>
2009-10-30 18:06:49 +0000

    bug 290631: [ScannerDiscovery61] Scanner Discovery Challenge Workspace list of language setting contributors

--- all/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/settings/model/LanguageSettingsExtensionManager.java
+++ all/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/settings/model/LanguageSettingsExtensionManager.java
@@ -12,11 +12,11 @@
 package org.eclipse.cdt.internal.core.settings.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.eclipse.cdt.core.CCorePlugin;
-import org.eclipse.cdt.core.errorparsers.RegexErrorParser;
 import org.eclipse.cdt.core.settings.model.ICLanguageSettingEntry;
 import org.eclipse.cdt.core.settings.model.ICLanguageSettingsContributor;
 import org.eclipse.cdt.core.settings.model.util.CDataUtil;
@@ -27,10 +27,16 @@ import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.osgi.service.prefs.BackingStoreException;
 
 public class LanguageSettingsExtensionManager {
+	private static final String PREFERENCE_CONTRIBUTOR_DEFAULT_IDS = "lang.settings.contributor.default.ids"; //$NON-NLS-1$
+	private static final String NONE = ""; //$NON-NLS-1$
+	private static final char CONTRIBUTOR_DELIMITER = ';';
 	/**
-	 * Name of the extension point for contributing an error parser
+	 * Name of the extension point for contributing language settings
 	 */
 	public final static String CONTRIBUTOR_EXTENSION_ID = "LanguageSettingsContributor"; //$NON-NLS-1$
 	private static final String ELEM_CONTRIBUTOR = "contributor"; //$NON-NLS-1$
@@ -46,29 +52,70 @@ public class LanguageSettingsExtensionManager {
 	private static final String ATTR_KIND = "kind"; //$NON-NLS-1$
 	private static final String ATTR_VALUE = "value"; //$NON-NLS-1$
 
-	private static final LinkedHashMap<String, ICLanguageSettingsContributor> globalLanguageSettingContributors = new LinkedHashMap<String, ICLanguageSettingsContributor>();
+	private static final LinkedHashMap<String, ICLanguageSettingsContributor> fExtensionContributors = new LinkedHashMap<String, ICLanguageSettingsContributor>();
+	private static final LinkedHashMap<String, ICLanguageSettingsContributor> fAvailableContributors = new LinkedHashMap<String, ICLanguageSettingsContributor>();
+	private static LinkedHashMap<String, ICLanguageSettingsContributor> fUserDefinedContributors = null;
+	private static List<String> fDefaultContributorIds = null;
 
 	static {
-		loadExtensions();
+//		loadUserDefinedContributors();
+		loadDefaultContributorIds();
+		loadContributorExtensions();
 	}
-
 
 
 	/**
-	 * Load error parser contributed extensions.
+	 * Load workspace default contributor IDs to be used if no contributors specified.
 	 *
 	 * @noreference This method is not intended to be referenced by clients.
 	 */
-	synchronized public static void loadExtensions() {
+	synchronized public static void loadDefaultContributorIds() {
+		fDefaultContributorIds = null;
+		IEclipsePreferences preferences = new InstanceScope().getNode(CCorePlugin.PLUGIN_ID);
+		String ids = preferences.get(PREFERENCE_CONTRIBUTOR_DEFAULT_IDS, NONE);
+		if (ids.equals(NONE)) {
+			return;
+		}
+
+		fDefaultContributorIds = Arrays.asList(ids.split(String.valueOf(CONTRIBUTOR_DELIMITER)));
+	}
+
+
+	/**
+	 * Load contributor contributed extensions.
+	 *
+	 * @noreference This method is not intended to be referenced by clients.
+	 */
+	synchronized public static void loadContributorExtensions() {
 
 		List<ICLanguageSettingsContributor> contributors = new ArrayList<ICLanguageSettingsContributor>();
-		loadExtensions(Platform.getExtensionRegistry(), contributors);
+		loadContributorExtensions(Platform.getExtensionRegistry(), contributors);
 
-		globalLanguageSettingContributors.clear();
+		fExtensionContributors.clear();
 		for (ICLanguageSettingsContributor contributor : contributors) {
-			globalLanguageSettingContributors.put(contributor.getId(), contributor);
+			fExtensionContributors.put(contributor.getId(), contributor);
+		}
+
+		recalculateAvailableContributors();
+	}
+
+	/**
+	 * Populate the list of available contributors where workspace level user defined parsers
+	 * overwrite contributed through contributor extension point.
+	 */
+	private static void recalculateAvailableContributors() {
+		fAvailableContributors.clear();
+		if (fUserDefinedContributors!=null) {
+			fAvailableContributors.putAll(fUserDefinedContributors);
+		}
+		for (ICLanguageSettingsContributor contributor : fExtensionContributors.values()) {
+			String id = contributor.getId();
+			if (!fAvailableContributors.containsKey(id)) {
+				fAvailableContributors.put(id, contributor);
+			}
 		}
 	}
+
 
 	/**
 	 * Load contributed extensions from extension registry.
@@ -76,7 +123,7 @@ public class LanguageSettingsExtensionManager {
 	 * @param registry - extension registry
 	 * @param contributors - resulting set of contributors
 	 */
-	private static void loadExtensions(IExtensionRegistry registry, List<ICLanguageSettingsContributor> contributors) {
+	private static void loadContributorExtensions(IExtensionRegistry registry, List<ICLanguageSettingsContributor> contributors) {
 		contributors.clear();
 		IExtensionPoint extension = registry.getExtensionPoint(CCorePlugin.PLUGIN_ID, CONTRIBUTOR_EXTENSION_ID);
 		if (extension != null) {
@@ -105,13 +152,13 @@ public class LanguageSettingsExtensionManager {
 	}
 
 	/**
-	 * Creates empty non-configured error parser as executable extension from extension point definition.
-	 * If "class" attribute is empty RegexErrorParser is created.
+	 * Creates empty non-configured contributor as executable extension from extension point definition.
+	 * If "class" attribute is empty TODO is created.
 	 *
-	 * @param initialId - nominal ID of error parser
-	 * @param initialName - nominal name of error parser
-	 * @param ce - configuration element with error parser definition
-	 * @return new non-configured error parser
+	 * @param initialId - nominal ID of contributor
+	 * @param initialName - nominal name of contributor
+	 * @param ce - configuration element with contributor definition
+	 * @return new non-configured contributor
 	 * @throws CoreException in case of failure
 	 */
 	private static ICLanguageSettingsContributor createExecutableContributor(IConfigurationElement ce) throws CoreException {
@@ -164,15 +211,15 @@ public class LanguageSettingsExtensionManager {
 	}
 
 	/**
-	 * Creates empty non-configured error parser from extension point definition looking at "class" attribute.
-	 * ID and name of error parser are assigned from first extension point encountered.
+	 * Creates empty non-configured contributor from extension point definition looking at "class" attribute.
+	 * ID and name of contributor are assigned from first extension point encountered.
 	 *
-	 * @param className - full qualified class name of error parser.
+	 * @param className - full qualified class name of contributor.
 	 * @param registry - extension registry
-	 * @return new non-configured error parser
+	 * @return new non-configured contributor
 	 */
 	private static ICLanguageSettingsContributor createContributorCarcass(String className, IExtensionRegistry registry) {
-		if (className==null || className.length()==0 || className.equals(RegexErrorParser.class.getName())) {
+		if (className==null || className.length()==0 /*|| className.equals(RegexContributor.class.getName())*/) {
 //			return new LanguageSettingsCoreContributor();
 			return null;
 		}
@@ -200,13 +247,149 @@ public class LanguageSettingsExtensionManager {
 
 
 	/**
-	 * @return IDs of language settings contributors of LanguageSettingContributor extension point.
+	 * FIXME
+	 * @param id - ID of contributor
+	 * @return cloned copy of contributor. Note that {@link ContributorNamedWrapper} returns
+	 * shallow copy with the same instance of underlying contributor.
 	 */
-	public static String[] getLanguageSettingsExtensionIds() {
-		return globalLanguageSettingContributors.keySet().toArray(new String[0]);
+	public static ICLanguageSettingsContributor getContributor(String id) {
+		ICLanguageSettingsContributor contributor = fAvailableContributors.get(id);
+
+//		try {
+//			if (contributor instanceof RegexContributor) {
+//				return (RegexContributor) ((RegexContributor)contributor).clone();
+//			} else if (contributor instanceof ContributorNamedWrapper) {
+//				return (ContributorNamedWrapper) ((ContributorNamedWrapper)contributor).clone();
+//			}
+//		} catch (CloneNotSupportedException e) {
+//			CCorePlugin.log(e);
+//		}
+		return contributor;
 	}
 
-	public static ICLanguageSettingsContributor getContributor(String id) {
-		return globalLanguageSettingContributors.get(id);
+	// FIXME:
+	public static List<ICLanguageSettingsContributor> getAllContributorsFIXME() {
+		return new ArrayList<ICLanguageSettingsContributor>(fExtensionContributors.values());
 	}
+
+
+	/**
+	 * @param ids - array of contributor IDs
+	 * @return contributor IDs delimited with contributor delimiter ";"
+	 * @since 5.2
+	 */
+	public static String toDelimitedString(String[] ids) {
+		String result=""; //$NON-NLS-1$
+		for (String id : ids) {
+			if (result.length()==0) {
+				result = id;
+			} else {
+				result += CONTRIBUTOR_DELIMITER + id;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Save the list of default contributors in preferences.
+	 *
+	 * @throws BackingStoreException in case of problem storing
+	 */
+	public static void serializeDefaultContributorIds() throws BackingStoreException {
+		IEclipsePreferences preferences = new InstanceScope().getNode(CCorePlugin.PLUGIN_ID);
+		String ids = NONE;
+		if (fDefaultContributorIds!=null) {
+			ids = toDelimitedString(fDefaultContributorIds.toArray(new String[0]));
+		}
+
+		preferences.put(PREFERENCE_CONTRIBUTOR_DEFAULT_IDS, ids);
+		preferences.flush();
+	}
+
+	/**
+	 * Set and store in workspace area user defined contributors.
+	 *
+	 * @param contributors - array of user defined contributors
+	 * @throws CoreException in case of problems
+	 */
+	public static void setUserDefinedContributors(ICLanguageSettingsContributor[] contributors) throws CoreException {
+		setUserDefinedContributorsInternal(contributors);
+//		serializeUserDefinedContributors();
+	}
+
+	/**
+	 * Internal method to set user defined contributors in memory.
+	 *
+	 * @noreference This method is not intended to be referenced by clients.
+	 * Use {@link #setUserDefinedContributors(ICLanguageSettingsContributor[])}.
+	 *
+	 * @param contributors - array of user defined contributors
+	 */
+	public static void setUserDefinedContributorsInternal(ICLanguageSettingsContributor[] contributors) {
+		if (contributors==null) {
+			fUserDefinedContributors = null;
+		} else {
+			fUserDefinedContributors= new LinkedHashMap<String, ICLanguageSettingsContributor>();
+			// set customized list
+			for (ICLanguageSettingsContributor contributor : contributors) {
+				fUserDefinedContributors.put(contributor.getId(), contributor);
+			}
+		}
+		recalculateAvailableContributors();
+	}
+
+	/**
+	 * @return available contributors IDs which include contributed through extension + user defined ones
+	 * from workspace
+	 */
+	public static String[] getContributorAvailableIds() {
+		return fAvailableContributors.keySet().toArray(new String[0]);
+	}
+
+	/**
+	 * @return IDs of language settings contributors of LanguageSettingContributor extension point.
+	 */
+	public static String[] getContributorExtensionIds() {
+		return fExtensionContributors.keySet().toArray(new String[0]);
+	}
+
+
+	/**
+	 * Set and store default contributors IDs to be used if contributor list is empty.
+	 *
+	 * @param ids - default contributors IDs
+	 * @throws BackingStoreException in case of problem with storing
+	 */
+	public static void setDefaultContributorIds(String[] ids) throws BackingStoreException {
+		setDefaultContributorIdsInternal(ids);
+//		serializeDefaultContributorIds();
+	}
+
+	/**
+	 * Set default contributors IDs in internal list.
+	 *
+	 * @noreference This method is not intended to be referenced by clients.
+	 * Use {@link #setDefaultContributorIds(String[])}.
+	 *
+	 * @param ids - default contributors IDs
+	 */
+	public static void setDefaultContributorIdsInternal(String[] ids) {
+		if (ids==null) {
+			fDefaultContributorIds = null;
+		} else {
+			fDefaultContributorIds = new ArrayList<String>(Arrays.asList(ids));
+		}
+	}
+
+	/**
+	 * @return default contributors IDs to be used if contributor list is empty.
+	 */
+	public static String[] getDefaultContributorIds() {
+		if (fDefaultContributorIds==null) {
+			return fAvailableContributors.keySet().toArray(new String[0]);
+		}
+		return fDefaultContributorIds.toArray(new String[0]);
+	}
+
+
 }

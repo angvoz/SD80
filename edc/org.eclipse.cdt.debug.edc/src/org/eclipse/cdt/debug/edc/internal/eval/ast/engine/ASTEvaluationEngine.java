22229c04373612f454fc10b307fc839740dd3813
kryall <kryall>
2009-11-13 21:12:07 +0000

    Fixed build for change in CPreprocessor.

--- edc/org.eclipse.cdt.debug.edc/src/org/eclipse/cdt/debug/edc/internal/eval/ast/engine/ASTEvaluationEngine.java
+++ edc/org.eclipse.cdt.debug.edc/src/org/eclipse/cdt/debug/edc/internal/eval/ast/engine/ASTEvaluationEngine.java
@@ -14,9 +14,10 @@ import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.parser.ISourceCodeParser;
 import org.eclipse.cdt.core.dom.parser.c.GCCScannerExtensionConfiguration;
 import org.eclipse.cdt.core.dom.parser.cpp.GPPParserExtensionConfiguration;
-import org.eclipse.cdt.core.parser.CodeReader;
+import org.eclipse.cdt.core.parser.FileContent;
 import org.eclipse.cdt.core.parser.IScanner;
 import org.eclipse.cdt.core.parser.IScannerInfo;
+import org.eclipse.cdt.core.parser.IncludeFileContentProvider;
 import org.eclipse.cdt.core.parser.NullLogService;
 import org.eclipse.cdt.core.parser.ParserLanguage;
 import org.eclipse.cdt.core.parser.ParserMode;
@@ -24,25 +25,22 @@ import org.eclipse.cdt.core.parser.ScannerInfo;
 import org.eclipse.cdt.debug.edc.internal.eval.ast.engine.instructions.InstructionSequence;
 import org.eclipse.cdt.debug.edc.internal.eval.ast.engine.instructions.Interpreter;
 import org.eclipse.cdt.debug.edc.internal.eval.ast.engine.instructions.InvalidExpression;
-import org.eclipse.cdt.internal.core.dom.NullCodeReaderFactory;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.GNUCPPSourceParser;
 import org.eclipse.cdt.internal.core.parser.scanner.CPreprocessor;
 import org.eclipse.core.runtime.CoreException;
 
+@SuppressWarnings("restriction")
 public class ASTEvaluationEngine {
 
 	public static final String UNKNOWN_TYPE = "<UNKNOWN>"; //$NON-NLS-1$
 
 	public InstructionSequence getCompiledExpression(String expression) {
 
-		CodeReader reader = new CodeReader(("void dummy_func() { " + expression + " ; }").toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-
+		FileContent reader = FileContent.create("<edc-expression>", ("void dummy_func() { " + expression + " ; }").toCharArray());
 		IScannerInfo scannerInfo = new ScannerInfo(); // creates an empty
 		// scanner info
-		IScanner scanner = new CPreprocessor(reader, scannerInfo, ParserLanguage.CPP, new NullLogService(),
-				GCCScannerExtensionConfiguration.getInstance(), NullCodeReaderFactory.getInstance());
-		ISourceCodeParser parser = new GNUCPPSourceParser(scanner, ParserMode.COMPLETE_PARSE, new NullLogService(),
-				GPPParserExtensionConfiguration.getInstance(), null);
+		IScanner scanner = new CPreprocessor(reader, scannerInfo, ParserLanguage.CPP, new NullLogService(), GCCScannerExtensionConfiguration.getInstance(), IncludeFileContentProvider.getEmptyFilesProvider());
+		ISourceCodeParser parser = new GNUCPPSourceParser(scanner, ParserMode.COMPLETE_PARSE, new NullLogService(), GPPParserExtensionConfiguration.getInstance(), null);
 		IASTTranslationUnit ast = parser.parse();
 
 		ASTInstructionCompiler visitor = new ASTInstructionCompiler(expression);

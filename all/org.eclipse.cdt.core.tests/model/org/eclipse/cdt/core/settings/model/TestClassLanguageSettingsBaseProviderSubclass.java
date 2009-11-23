156dd6926fdb193a0e42032ccc30b2923f229238
agvozdev <agvozdev>
2009-11-23 18:41:31 +0000

    bug 290631: [ScannerDiscovery61] Scanner Discovery Challenge LanguageSettingsBaseProvider subclassed extensions

---
+++ all/org.eclipse.cdt.core.tests/model/org/eclipse/cdt/core/settings/model/TestClassLanguageSettingsBaseProviderSubclass.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2009 Andrew Gvozdev (Quoin Inc.) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Andrew Gvozdev (Quoin Inc.) - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.cdt.core.settings.model;
+
+import org.eclipse.cdt.internal.core.settings.model.LanguageSettingsBaseProvider;
+
+public class TestClassLanguageSettingsBaseProviderSubclass extends LanguageSettingsBaseProvider {
+
+}

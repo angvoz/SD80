0eb2735fdb1938f200ae38093d60580778e4a02b
mkhouzam <mkhouzam>
2009-12-16 20:32:35 +0000

    [275687] Moving Reverse commands to the core of CDT from DSF-GDB, and making use of the new asynchronous command framework of the debug platform

---
+++ all/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/commands/ReverseStepIntoCommandHandler.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Ericsson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Ericsson - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.debug.internal.ui.commands;
+
+import org.eclipse.cdt.debug.core.model.IReverseStepIntoHandler;
+import org.eclipse.debug.ui.actions.DebugCommandHandler;
+
+/**
+ * Command handler to trigger a reverse stepinto operation
+ * 
+ * @since 6.1
+ */
+public class ReverseStepIntoCommandHandler extends DebugCommandHandler {
+    @Override
+    protected Class<?> getCommandType() {
+        return IReverseStepIntoHandler.class;
+    }
+}

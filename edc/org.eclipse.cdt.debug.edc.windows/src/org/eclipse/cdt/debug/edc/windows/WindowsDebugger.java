d29c326f735fae054d8d2121d9a0ba87c4c01b8b
jcortell <jcortell>
2010-01-21 00:17:41 +0000

    Fixed warning

--- edc/org.eclipse.cdt.debug.edc.windows/src/org/eclipse/cdt/debug/edc/windows/WindowsDebugger.java
+++ edc/org.eclipse.cdt.debug.edc.windows/src/org/eclipse/cdt/debug/edc/windows/WindowsDebugger.java
@@ -124,7 +124,7 @@ public class WindowsDebugger extends Plugin {
 			final String workingDirectory = cfg.getAttribute(ICDTLaunchConfigurationConstants.ATTR_WORKING_DIRECTORY,
 					"");
 			final String args = cfg.getAttribute(ICDTLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, "");
-			final Map<String, String> env = cfg.getAttribute(ILaunchManager.ATTR_ENVIRONMENT_VARIABLES, (Map) null);
+			final Map<String, String> env = cfg.getAttribute(ILaunchManager.ATTR_ENVIRONMENT_VARIABLES, (Map<?,?>) null);
 			final boolean append = cfg.getAttribute(ILaunchManager.ATTR_APPEND_ENVIRONMENT_VARIABLES, true);
 			final boolean attach = true;
 

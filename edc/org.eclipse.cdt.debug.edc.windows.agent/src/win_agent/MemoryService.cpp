0907bef7dba47bce254dcce36689ee5971d5c02d
kryall <kryall>
2010-02-11 20:42:32 +0000

    Fix NPE.

--- edc/org.eclipse.cdt.debug.edc.windows.agent/src/win_agent/MemoryService.cpp
+++ edc/org.eclipse.cdt.debug.edc.windows.agent/src/win_agent/MemoryService.cpp
@@ -77,7 +77,6 @@ void MemoryService::command_get(char * token, Channel * c) {
 	TCFChannel channel(c);
 	std::string id = channel.readString();
 	channel.readZero();
-	Context* context = ContextManager::FindDebuggedContext(id);
 	unsigned long address = channel.readULong();
 	channel.readZero();
 	long wordSize = channel.readLong();
@@ -92,8 +91,13 @@ void MemoryService::command_get(char * token, Channel * c) {
 	char* memBuffer = new char[size];
 	int memBufferSize = size;
 
-	int error = context->ReadMemory(address, size, memBuffer, memBufferSize,
-			bytesRead);
+	Context* context = ContextManager::FindDebuggedContext(id);
+
+	if (context != NULL)
+	{
+		int error = context->ReadMemory(address, size, memBuffer, memBufferSize,
+				bytesRead);
+	}
 
 	channel.writeReplyHeader(token);
 	channel.writeBinaryData(memBuffer, memBufferSize);

4042029221743ab1a4f55377ff6f2f567e8a7a92
jcortell <jcortell>
2010-03-10 19:40:17 +0000

    [305243] The Delete command should be disabled when there are no albums or snapshots

---
+++ edc/org.eclipse.cdt.debug.edc.ui/src/org/eclipse/cdt/debug/edc/internal/ui/SnapshotViewPropertyTester.java
@@ -0,0 +1,40 @@
+package org.eclipse.cdt.debug.edc.internal.ui;
+
+import org.eclipse.cdt.debug.edc.internal.snapshot.Album;
+import org.eclipse.cdt.debug.edc.internal.snapshot.Snapshot;
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TreeNode;
+
+/**
+ * A property tester used to programmatically test the type of the selection in
+ * the Snapshots view to see if it's an Album or a Snapshot. This can't be done
+ * declaratively via 'instanceof' because the elements of the view are TreeNode
+ * objects.
+ * 
+ * This property tester is referenced by using a 'test' element in the command
+ * enablement expression with a property of
+ * "org.eclipse.cdt.debug.edc.ui.isAlbumOrSnapshot".
+ */
+public class SnapshotViewPropertyTester extends PropertyTester {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.expressions.IPropertyTester#test(java.lang.Object, java.lang.String, java.lang.Object[], java.lang.Object)
+	 */
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+		if (property.equals("isAlbumOrSnapshot")) {
+			IStructuredSelection selection = (IStructuredSelection)receiver;
+			Object selectionElement = selection.getFirstElement();
+			if (selectionElement instanceof TreeNode) {
+				TreeNode treeNode = (TreeNode)selectionElement;
+				Object element = treeNode.getValue();
+				return (element instanceof Album) || (element instanceof Snapshot);
+			}
+		}
+		else {
+			assert false : "unexpected property; check for a typo in the enablement expression";
+		}
+		return false;
+	}
+
+}

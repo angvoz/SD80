660ac0d4121b8deb2ee716dcb4786f09b7a0fc07
elaskavaia <elaskavaia>
2010-03-18 14:04:46 +0000

    separate language specific plugins from general

---
+++ codan/org.eclipse.cdt.codan.ui.cxx/src/org/eclipse/cdt/codan/internal/ui/cxx/CodanCReconciler.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Alena Laskavaia 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Alena Laskavaia  - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.codan.internal.ui.cxx;
+
+import org.eclipse.cdt.codan.core.cxx.internal.model.CxxCodanRenciler;
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.internal.ui.editor.CEditor;
+import org.eclipse.cdt.internal.ui.text.ICReconcilingListener;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+/**
+ * @author Alena
+ * 
+ */
+public class CodanCReconciler implements ICReconcilingListener {
+	private CxxCodanRenciler reconsiler = new CxxCodanRenciler();
+
+	void install(ITextEditor editor) {
+		if (editor instanceof CEditor) {
+			initialize();
+			((CEditor) editor).addReconcileListener(this);
+		}
+	}
+
+	void uninstall(ITextEditor editor) {
+		if (editor instanceof CEditor) {
+			initialize();
+			((CEditor) editor).removeReconcileListener(this);
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private void initialize() {
+		// nothing?
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.cdt.internal.ui.text.ICReconcilingListener#aboutToBeReconciled
+	 * ()
+	 */
+	public void aboutToBeReconciled() {
+		// nothing
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.cdt.internal.ui.text.ICReconcilingListener#reconciled(org
+	 * .eclipse.cdt.core.dom.ast.IASTTranslationUnit, boolean,
+	 * org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void reconciled(IASTTranslationUnit ast, boolean force,
+			IProgressMonitor progressMonitor) {
+		reconsiler.reconciledAst(ast, progressMonitor);
+		// System.err.println("ast reconsiled");
+	}
+}

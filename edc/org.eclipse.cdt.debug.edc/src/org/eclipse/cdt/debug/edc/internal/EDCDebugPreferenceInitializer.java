df696c4f4530820c18ba8566f8f1b36b118649a6
kryall <kryall>
2010-04-13 16:50:48 +0000

    Fix problems syncing the variable formatter button and the underlying pref setting.

--- edc/org.eclipse.cdt.debug.edc/src/org/eclipse/cdt/debug/edc/internal/EDCDebugPreferenceInitializer.java
+++ edc/org.eclipse.cdt.debug.edc/src/org/eclipse/cdt/debug/edc/internal/EDCDebugPreferenceInitializer.java
@@ -11,6 +11,7 @@
 package org.eclipse.cdt.debug.edc.internal;
 
 import org.eclipse.cdt.debug.edc.EDCDebugger;
+import org.eclipse.cdt.debug.edc.internal.formatter.FormatExtensionManager;
 import org.eclipse.cdt.debug.edc.internal.snapshot.Album;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.core.runtime.preferences.DefaultScope;
@@ -24,6 +25,7 @@ public class EDCDebugPreferenceInitializer extends
 		IEclipsePreferences node = new DefaultScope().getNode(EDCDebugger.PLUGIN_ID);
 		node.putInt(Album.PREF_VARIABLE_CAPTURE_DEPTH, 5);
 		node.put(Album.PREF_CREATION_CONTROL, Album.CREATE_MANUAL);
+		node.putBoolean(FormatExtensionManager.VARIABLE_FORMATS_ENABLED, FormatExtensionManager.VARIABLE_FORMATS_ENABLED_DEFAULT);
 	}
 
 }

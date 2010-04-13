72c6841f2ecffa99d94f7ebb18d6a1f5fe4cc0ce
kryall <kryall>
2010-04-13 16:50:47 +0000

    Fix problems syncing the variable formatter button and the underlying pref setting.

--- edc/org.eclipse.cdt.debug.edc.ui/src/org/eclipse/cdt/debug/edc/internal/ui/actions/ToggleCustomFormattingCommandHandler.java
+++ edc/org.eclipse.cdt.debug.edc.ui/src/org/eclipse/cdt/debug/edc/internal/ui/actions/ToggleCustomFormattingCommandHandler.java
@@ -12,6 +12,7 @@ package org.eclipse.cdt.debug.edc.internal.ui.actions;
 
 import org.eclipse.cdt.debug.edc.EDCDebugger;
 import org.eclipse.cdt.debug.edc.internal.formatter.FormatExtensionManager;
+import org.eclipse.cdt.debug.edc.ui.EDCDebugUI;
 import org.eclipse.cdt.dsf.ui.viewmodel.IVMAdapter;
 import org.eclipse.cdt.dsf.ui.viewmodel.IVMProvider;
 import org.eclipse.cdt.dsf.ui.viewmodel.update.ICachingVMProvider;
@@ -19,6 +20,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.State;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.InstanceScope;
@@ -29,20 +31,44 @@ import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.debug.ui.contexts.IDebugContextService;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.handlers.RegistryToggleState;
 import org.osgi.service.prefs.BackingStoreException;
 
 @SuppressWarnings("restriction")
 public class ToggleCustomFormattingCommandHandler extends AbstractHandler {
+
+	public ToggleCustomFormattingCommandHandler() {
+		super();
+		IWorkbench workbench = PlatformUI.getWorkbench();
+		ICommandService commandSupport = (ICommandService)workbench.getAdapter(ICommandService.class);
+		if (commandSupport != null)
+		{
+			Command command = commandSupport.getCommand("org.eclipse.cdt.debug.edc.ui.toggleCustomFormatting");
+			if (command != null)
+			{
+				try {
+					State state = command.getState(RegistryToggleState.STATE_ID);
+					if(state != null)
+						state.setValue(new Boolean(FormatExtensionManager.instance().isEnabled()));
+				} catch (Exception e) {
+					EDCDebugUI.getMessageLogger().logError(null, e);
+				}
+			}
+		}
+	}
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		Command command = event.getCommand();
 		boolean enabled = HandlerUtil.toggleCommandState(command);
 		FormatExtensionManager.instance().setEnabled(!enabled );
 		IEclipsePreferences scope = new InstanceScope().getNode(EDCDebugger.PLUGIN_ID);
-		scope.putBoolean(FormatExtensionManager.VARIABLE_FORMATS_ENABLED, enabled);
+		scope.putBoolean(FormatExtensionManager.VARIABLE_FORMATS_ENABLED, !enabled);
 		try {
 			scope.flush();
 		} catch (BackingStoreException e) {

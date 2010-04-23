ad3aa79163cf93fb40259196f879aeadd44c5041
lwang <lwang>
2010-04-23 06:04:12 +0000

    code cleanup and comment revision.

--- edc/org.eclipse.cdt.debug.edc.tcf.extension/src/org/eclipse/cdt/debug/edc/tcf/extension/ProtocolConstants.java
+++ edc/org.eclipse.cdt.debug.edc.tcf.extension/src/org/eclipse/cdt/debug/edc/tcf/extension/ProtocolConstants.java
@@ -24,26 +24,28 @@ public interface ProtocolConstants {
 	static final String PROP_OS_ID = "OSID"; // value type: string
 
 	/*
-	 * These are properties that a TCF agent should report with module-load or
-	 * module-unload events. They are all optional. But if relocation does
-	 * happen (e.g. for a Windows DLL), the agent must report at least
-	 * PROP_IMAGE_BASE_ADDRESS or PROP_CODE_ADDRESS.
+	 * These are properties that a TCF agent should report with module (a
+	 * library or executable) load or unload events. They are not all required.
+	 * But if relocation does happen (e.g. for a Windows DLL), the agent must
+	 * report at least PROP_IMAGE_BASE_ADDRESS or PROP_CODE_ADDRESS.
 	 */
 	public static interface IModuleProperty {
-		public static final String PROP_FILE = "File", // module file name
-				PROP_TIME = "Time", PROP_MODULE_LOADED = "Loaded", // Boolean,
-				// whether
-				// loaded or
-				// unloaded.
+		public static final String 
+				PROP_FILE = "File", // module file name
+				
+				PROP_TIME = "Time", 
+
+				// whether the module is loaded or unloaded.
+				// Value type: boolean
+				PROP_MODULE_LOADED = "Loaded", 
 
 				PROP_CODE_SIZE = "CodeSize", // value type: Number.
 				PROP_DATA_SIZE = "DataSize", // value type: Number.
 				PROP_BSS_SIZE = "BssSize", // value type: Number.
 
 				// image base address for modules (PE file) from Windows
-				PROP_IMAGE_BASE_ADDRESS = "ImageBaseAddress", // value type:
-				// Number (Java
-				// "Number").
+				// value type: Number (Java "Number").
+				PROP_IMAGE_BASE_ADDRESS = "ImageBaseAddress", 
 
 				// following are for systems that can give us such info.
 				// Note these are mutually exclusive with
@@ -51,6 +53,13 @@ public interface ProtocolConstants {
 				PROP_CODE_ADDRESS = "CodeAddress", // value type: Number.
 				PROP_DATA_ADDRESS = "DataAddress", // value type: Number.
 				PROP_BSS_ADDRESS = "BssAddress", // value type: Number.
+				
+				// This property indicates whether host should send a resume
+				// command after handling a module load/unload event.
+				// Usually agent knows if the debugged process is suspended
+				// when the load/unload event happens, thus it's agent's duty
+				// to set this property. 
+				// By default, host assumes that resume is needed.
 				PROP_RESUME = "RequireResume"; // value type: Boolean
 	};
 

78ddb35060bd09605e6e9521fe1c02938fd650ba
mkhouzam <mkhouzam>
2010-05-14 01:50:34 +0000

    Bug 281970: No longer show the Debugger, Refresh and Source tabs for a Run Configuration.  Also, make sure a Run configuration can be used for Debug and vice versa.

--- edc/org.eclipse.cdt.debug.edc.ui/src/org/eclipse/cdt/debug/edc/internal/ui/launch/RegularDebuggerTab.java
+++ edc/org.eclipse.cdt.debug.edc.ui/src/org/eclipse/cdt/debug/edc/internal/ui/launch/RegularDebuggerTab.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Nokia and others.
+ * Copyright (c) 2009, 2010 Nokia and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,10 +10,26 @@
  *******************************************************************************/
 package org.eclipse.cdt.debug.edc.internal.ui.launch;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+
 public class RegularDebuggerTab extends AbstractDebuggerTab {
 	private static final String TAB_ID = "org.eclipse.cdt.debug.edc.ui.launch.regularDebuggerTab";
 	private static final String HELP_ID = "org.eclipse.cdt.debug.edc.ui.launch_regularDebuggerTab";
 
+	/*
+	 * When the launch configuration is created for Run mode,
+	 * this Debugger tab is not created because it is not used
+	 * for Run mode but only for Debug mode.
+	 * When we then open the same configuration in Debug mode, the launch
+	 * configuration already exists and initializeFrom() is called
+	 * instead of setDefaults().
+	 * We therefore call setDefaults() ourselves.
+	 * Bug 281970
+	 */
+	private boolean fSetDefaultCalled;
+	
 	public RegularDebuggerTab() {
 		super(false);
 	}
@@ -27,4 +43,26 @@ public class RegularDebuggerTab extends AbstractDebuggerTab {
 	protected String getTabID() {
 		return TAB_ID;
 	}
+	
+    @Override
+    public void setDefaults(ILaunchConfigurationWorkingCopy config) {
+    	fSetDefaultCalled = true;
+    	
+    	super.setDefaults(config);
+    }
+    
+    @Override
+    public void initializeFrom(ILaunchConfiguration config) {
+		if (fSetDefaultCalled == false) {
+			try {
+				ILaunchConfigurationWorkingCopy wc;
+				wc = config.getWorkingCopy();
+				setDefaults(wc);
+				wc.doSave();
+			} catch (CoreException e) {
+			}
+		}
+
+		super.initializeFrom(config);
+    }
 }

12af01083a3568619791a16c8676d81d1b086e46
kryall <kryall>
2010-07-07 16:01:07 +0000

    Bug 319151 -  Multi launch improvements

--- edc/org.eclipse.cdt.debug.edc.windows/src/org/eclipse/cdt/debug/edc/windows/launch/WindowsLaunchDelegate.java
+++ edc/org.eclipse.cdt.debug.edc.windows/src/org/eclipse/cdt/debug/edc/windows/launch/WindowsLaunchDelegate.java
@@ -10,6 +10,10 @@
  *******************************************************************************/
 package org.eclipse.cdt.debug.edc.windows.launch;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.cdt.debug.edc.internal.EDCDebugger;
 import org.eclipse.cdt.debug.edc.launch.EDCLaunch;
 import org.eclipse.cdt.debug.edc.launch.EDCLaunchDelegate;
 import org.eclipse.cdt.debug.edc.windows.WindowsDebugger;
@@ -17,7 +21,11 @@ import org.eclipse.cdt.debug.edc.x86.DebugServicesFactoryX86;
 import org.eclipse.cdt.dsf.concurrent.DsfExecutor;
 import org.eclipse.cdt.dsf.concurrent.Sequence;
 import org.eclipse.cdt.dsf.debug.service.IDsfDebugServicesFactory;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchDelegate;
 
 public class WindowsLaunchDelegate extends EDCLaunchDelegate {
 
@@ -41,6 +49,30 @@ public class WindowsLaunchDelegate extends EDCLaunchDelegate {
 	@Override
 	protected String getPluginID() {
 		return WindowsDebugger.getUniqueIdentifier();
+	}
+
+	@Override
+	protected boolean isSameTarget(EDCLaunch existingLaunch,
+			ILaunchConfiguration configuration, String mode) {
+		try {
+			ILaunchConfiguration existingConfiguration = existingLaunch.getLaunchConfiguration();
+			ILaunchConfigurationType existingType = existingConfiguration.getType();
+			ILaunchConfigurationType newType = configuration.getType();
+			if (existingType.equals(newType))
+			{
+				Set<String> modes = new HashSet<String>();
+				modes.add(mode);
+				ILaunchDelegate existingDelegate = existingConfiguration.getPreferredDelegate(modes);
+				ILaunchDelegate delegate = configuration.getPreferredDelegate(modes);
+				if (existingDelegate.equals(delegate))
+				{
+					return true;
+				}
+			}
+		} catch (CoreException e) {
+			EDCDebugger.getMessageLogger().logError(null, e);
+		}
+		return false;
 	}
 
 }

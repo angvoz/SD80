229eeabe5a0d8e8e8e3fc0b57b07397dbe411d1f
kryall <kryall>
2010-08-30 18:50:38 +0000

    Bug 323457 -  edc - setting breakpoint on line without source fails to resolve at subsequent line

--- edc/org.eclipse.cdt.debug.edc/src/org/eclipse/cdt/debug/edc/internal/services/dsf/LineEntryMapper.java
+++ edc/org.eclipse.cdt.debug.edc/src/org/eclipse/cdt/debug/edc/internal/services/dsf/LineEntryMapper.java
@@ -33,7 +33,9 @@ import org.eclipse.core.runtime.IPath;
  * Utilities for mapping source to addresses and vice versa.
  */
 public class LineEntryMapper {
-	
+
+	private static final int toEOF = -1;
+
 	/**
 	 * Get the link-time address ranges starting at the given source location.  
 	 * There may be more than one, e.g., in template expansions, static functions
@@ -47,23 +49,61 @@ public class LineEntryMapper {
 			IModuleLineEntryProvider moduleLineEntryProvider,
 			IPath sourceFile,
 			int line) {
+		Collection<AddressRange> range = getAddressRangesAtSource(moduleLineEntryProvider, sourceFile, line, line);
+		if (range.isEmpty()) {
+			range = getAddressRangesAtSource(moduleLineEntryProvider, sourceFile, line, toEOF);
+		}
+		return range;
+	}
+
+	/**
+	 * The public version of this function acts as a wrapper, allowing the original algorithm
+	 * to try all fileProviders first, and if that fails, to try to find the next line.
+	 * The use case in the original case is to be precise about inline/template situations.
+	 * The use case in the new version of the call is to set a breakpoint on first available line
+	 * in case the user has chosen to set a breakpoint on a line without LNT information.
+	 * @path sourceFile the absolute path to the source file
+	 * @param startLine the line number (1-based) 
+	 * @param endLine the line number (1-based) 
+	 * @return the unmodifiable list of link-time addresses which start at the given line, possibly empty.
+	 */
+	private static Collection<AddressRange> getAddressRangesAtSource(
+			IModuleLineEntryProvider moduleLineEntryProvider,
+			IPath sourceFile,
+			int startLine,
+			int endLine) {
 		Collection<? extends ILineEntryProvider> fileProviders = 
 			moduleLineEntryProvider.getLineEntryProvidersForFile(sourceFile);
 		if (fileProviders.isEmpty())
 			return Collections.emptyList();
-			
+
 		int lastColumn = -1;
 		IPath lastFile = null;
-		
+		int bestLine = endLine;
+
 		List<EDCAddressRange> addrRanges = null;
 		for (ILineEntryProvider fileProvider : fileProviders) {
 			
-			Collection<ILineEntry> entries = fileProvider.getLineEntriesForLines(sourceFile, line, line);
-			
-			if (addrRanges == null)
+			Collection<ILineEntry> entries = fileProvider.getLineEntriesForLines(sourceFile, startLine, endLine);
+			if (addrRanges == null && !entries.isEmpty())
 				addrRanges = new ArrayList<EDCAddressRange>();
 			
 			for (ILineEntry entry : entries) {
+
+				int entryLine = entry.getLineNumber();
+
+				if (entryLine < bestLine) {
+					addrRanges.clear();
+					bestLine = entryLine;
+				} 
+				else if (bestLine == toEOF) {
+					bestLine = entryLine;
+				}
+				else if (entryLine > bestLine) {
+					break;	// assume entries sorted; go onto the next fileProvider
+				}
+				// else (entryLine == bestLine) // implied
+
 				/*
 				 * when there is more than one line mapping for the source line,
 				 * see if it makes sense to merge the line entries into the same
@@ -75,22 +115,17 @@ public class LineEntryMapper {
 				 * the column will be the same. for these cases it makes sense
 				 * to keep the address ranges separate.
 				 */
-				boolean addNewRange = true;
-				if (addrRanges.size() > 0 && entry.getFilePath().equals(lastFile) && lastColumn != entry.getColumnNumber()) {
-					addNewRange = false;
-				}
-				
-				if (addNewRange) {
-					EDCAddressRange range = new EDCAddressRange(entry.getLowAddress(), entry.getHighAddress());
-					addrRanges.add(range);
+				IPath entryPath = entry.getFilePath();
+				int entryColumn = entry.getColumnNumber();
+				if (addrRanges.isEmpty() || !entryPath.equals(lastFile) || lastColumn == entryColumn) {
+					addrRanges.add(new EDCAddressRange(entry.getLowAddress(), entry.getHighAddress()));
 				} else {
 					EDCAddressRange range = addrRanges.remove(addrRanges.size() - 1);
 					range.setEndAddress(entry.getHighAddress());
 					addrRanges.add(range);
 				}
-				
-				lastColumn = entry.getColumnNumber();
-				lastFile = entry.getFilePath();
+				lastColumn = entryColumn;
+				lastFile = entryPath;
 			}
 		}
 		

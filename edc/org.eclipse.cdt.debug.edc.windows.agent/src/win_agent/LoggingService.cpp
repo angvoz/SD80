5b86e5707841934142cc4df47701d110f2d7e129
kryall <kryall>
2010-09-01 04:59:39 +0000

    Bug 323997 -  edc windows agent - add capability to use new dialog() in ILogging

--- edc/org.eclipse.cdt.debug.edc.windows.agent/src/win_agent/LoggingService.cpp
+++ edc/org.eclipse.cdt.debug.edc.windows.agent/src/win_agent/LoggingService.cpp
@@ -88,6 +88,18 @@ struct LoggingMessage {
 	{ }
 };
 
+struct DialogMessage {
+	Channel* channel;
+	IStatus::Severity severity;
+	std::string summary;
+	std::string details;
+
+	DialogMessage(Channel* channel_, const IStatus::Severity severity,
+					const std::string& summary_, const std::string& details_)
+	  : channel(channel_), severity(severity), summary(summary_), details(details_)
+	{ }
+};
+
 static void emit_logging_message(void *data) {
 	LoggingMessage* m = (LoggingMessage*) data;
 	
@@ -103,13 +115,62 @@ static void emit_logging_message(void *data) {
 	tcf.writeZero();
 	tcf.writeString(m->str);
 	tcf.writeZero();
+
 	tcf.writeComplete();
 	
 	delete m;
 }
 
-// Currently on sends "write" event. Another method could be send to add a newline with the "writeln" event.
-void LoggingService::WriteLoggingMessage(Channel *channel, std::string str, const char *consoleID)
+
+static void emit_logging_message_with_newline(void *data) {
+	LoggingMessage* m = (LoggingMessage*) data;
+
+	TCFChannel tcf(m->channel);
+
+	// write to the console
+	tcf.writeStringZ("E");
+	tcf.writeStringZ(sServiceName);
+	tcf.writeStringZ("writeln");
+
+	/* <array of context data> */
+	tcf.writeString(m->consoleID);
+	tcf.writeZero();
+	tcf.writeString(m->str);
+	tcf.writeZero();
+
+	tcf.writeComplete();
+
+	delete m;
+}
+
+
+static void emit_dialog_message(void *data) {
+	DialogMessage* m = (DialogMessage*) data;
+
+	TCFChannel tcf(m->channel);
+
+	// write to the console
+	tcf.writeStringZ("E");
+	tcf.writeStringZ(sServiceName);
+	tcf.writeStringZ("dialog");
+
+	/* <array of context data> */
+	tcf.writeString(LoggingService::GetWindowsConsoleID());
+	tcf.writeZero();
+	tcf.writeLong((long)m->severity);
+	tcf.writeZero();
+	tcf.writeString(m->summary);
+	tcf.writeZero();
+	tcf.writeString(m->details);
+	tcf.writeZero();
+
+	tcf.writeComplete();
+
+	delete m;
+}
+
+
+void LoggingService::WriteLoggingMessage(Channel *channel, const std::string& str, const char *consoleID)
 {
 	const char *windowsConsoleID = GetWindowsConsoleID();
 	// only send messages to the proper console service and when there are more than one listener
@@ -118,4 +179,23 @@ void LoggingService::WriteLoggingMessage(Channel *channel, std::string str, cons
 		LoggingMessage* message = new LoggingMessage(channel, str, consoleID);
 		post_event(emit_logging_message, message);
 	}
+}
+
+/** since 2.0 */
+void LoggingService::WriteLnLoggingMessage(Channel *channel, const std::string& str, const char *consoleID)
+{
+	const char *windowsConsoleID = GetWindowsConsoleID();
+	// only send messages to the proper console service and when there are more than one listener
+	if ( numConsoleListeners > 0 && strcmp(windowsConsoleID, consoleID) == 0 )
+	{
+		LoggingMessage* message = new LoggingMessage(channel, str, consoleID);
+		post_event(emit_logging_message_with_newline, message);
+	}
+}
+
+/** since 2.0 */
+void LoggingService::Dialog(Channel *channel, IStatus::Severity severity, const std::string& summary, const std::string& details)
+{
+	DialogMessage* message = new DialogMessage(channel, severity, summary, details);
+	post_event(emit_dialog_message, message);
 }

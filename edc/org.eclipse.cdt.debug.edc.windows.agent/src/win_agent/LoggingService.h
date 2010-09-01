d1809d2621b4e20f27fdff8a30fa214b15a41e02
kryall <kryall>
2010-09-01 04:59:39 +0000

    Bug 323997 -  edc windows agent - add capability to use new dialog() in ILogging

--- edc/org.eclipse.cdt.debug.edc.windows.agent/src/win_agent/LoggingService.h
+++ edc/org.eclipse.cdt.debug.edc.windows.agent/src/win_agent/LoggingService.h
@@ -11,13 +11,43 @@
 #ifndef LOGGINGSERVICE_H_
 #define LOGGINGSERVICE_H_
 
-
 #include "TCFService.h"
 #include "TCFHeaders.h"
 
+namespace IStatus { 	/** from IStatus.java */
+	enum Severity {
+
+// IStatus.OK not currently accepted by ILogging.Dialog()
+//		/** Status severity constant (value 0) indicating this status represents the nominal case.
+//		 * This constant is also used as the status code representing the nominal case.
+//		 */
+//		OK = 0,
+
+		/** Status type severity (bit mask, value 1) indicating this status is informational only.
+		 */
+		INFO = 0x01,
+
+		/** Status type severity (bit mask, value 2) indicating this status represents a warning.
+		 */
+		WARNING = 0x02,
+
+		/** Status type severity (bit mask, value 4) indicating this status represents an error.
+		 */
+		_ERROR = 0x04, /// ERROR without _ disallowed due to macro conflict
+
+// IStatus.CANCEL not currently accepted by ILogging.Dialog()
+//		/** Status type severity (bit mask, value 8) indicating this status represents a
+//		 * cancelation
+//		 * @since 3.0
+//		 */
+//		CANCEL = 0x08,
+	};
+};
+
 class LoggingService: public TCFService {
 public:
-    LoggingService(Protocol * proto);
+
+	LoggingService(Protocol * proto);
 	~LoggingService(void);
 
 	const char* GetName();
@@ -26,7 +56,13 @@ public:
 	static void command_removeListener(const char * token, Channel * c);
 
 	static void sendOK(const char * token, Channel * c);
-	static void WriteLoggingMessage(Channel *channel, std::string str, const char *consoleID);
+	static void WriteLoggingMessage(Channel *channel, const std::string& str, const char *consoleID);
+
+	/** since 2.0 */
+	static void WriteLnLoggingMessage(Channel *channel, const std::string& str, const char *consoleID);
+	/** since 2.0 */
+	static void Dialog(Channel *channel, IStatus::Severity severity, const std::string& summary, const std::string& details);
+
 	static const char * GetWindowsConsoleID();
 
 private:

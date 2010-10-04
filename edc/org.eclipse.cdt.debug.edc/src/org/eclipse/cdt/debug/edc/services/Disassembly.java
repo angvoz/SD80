31bdac87af927ca919c8ca564dc50f6f11fa3872
kryall <kryall>
2010-10-04 20:55:10 +0000

    Fixed potential array index out of bounds exception that would leave the disassembly view hosed.

--- edc/org.eclipse.cdt.debug.edc/src/org/eclipse/cdt/debug/edc/services/Disassembly.java
+++ edc/org.eclipse.cdt.debug.edc/src/org/eclipse/cdt/debug/edc/services/Disassembly.java
@@ -14,12 +14,16 @@ import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.eclipse.cdt.core.IAddress;
+import org.eclipse.cdt.debug.edc.disassembler.CodeBufferUnderflowException;
+import org.eclipse.cdt.debug.edc.disassembler.DisassembledInstruction;
 import org.eclipse.cdt.debug.edc.disassembler.EDCInstruction;
+import org.eclipse.cdt.debug.edc.disassembler.EDCInstructionFunctionInfo;
 import org.eclipse.cdt.debug.edc.disassembler.EDCMixedInstruction;
 import org.eclipse.cdt.debug.edc.disassembler.IDisassembledInstruction;
 import org.eclipse.cdt.debug.edc.disassembler.IDisassembler;
@@ -28,6 +32,7 @@ import org.eclipse.cdt.debug.edc.internal.services.dsf.EDCServicesMessages;
 import org.eclipse.cdt.debug.edc.launch.EDCLaunch;
 import org.eclipse.cdt.debug.edc.symbols.ILineEntry;
 import org.eclipse.cdt.dsf.concurrent.DataRequestMonitor;
+import org.eclipse.cdt.dsf.concurrent.RequestMonitor;
 import org.eclipse.cdt.dsf.datamodel.DMContexts;
 import org.eclipse.cdt.dsf.debug.service.IDisassembly;
 import org.eclipse.cdt.dsf.debug.service.IInstruction;
@@ -41,6 +46,7 @@ import org.eclipse.cdt.dsf.service.DsfServicesTracker;
 import org.eclipse.cdt.dsf.service.DsfSession;
 import org.eclipse.cdt.utils.Addr64;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.model.MemoryByte;
@@ -61,33 +67,93 @@ public class Disassembly extends AbstractEDCService implements IDisassembly {
 	}
 
 	/**
-	 * @return NLS-ized string indicating the there's no disassembler, yet
+	 * @return IStatus.ERROR containing NLS string indicating disassembler not yet available
 	 * @since 2.0
 	 */
-	protected IStatus statusNoDisassembler() {
+	public static IStatus statusNoDisassembler() {
 		return new Status(IStatus.ERROR, EDCDebugger.PLUGIN_ID, REQUEST_FAILED,
 						  EDCServicesMessages.Disassembly_NoDisassemblerYet, null);
 	}
 
 	/**
-	 * @return NLS-ized string indicating cannot read memory, yet
+	 * @param memoryAt 1st location of unreadable memory
+	 * @param memoryLength length of unreadable memory
+	 * @return IStatus.ERROR containing formatted message with location & length
+	 */
+	private static IStatus statusCannotReadMemory(String memoryAt, Integer memoryLength) {
+		return new Status(IStatus.ERROR, EDCDebugger.PLUGIN_ID, REQUEST_FAILED,
+						  cantReadMemory(memoryAt, memoryLength.toString()), null);
+	}
+
+	/**
+	 * string for use in both status for error return and in
+	 * pseudo-instruction used in error recovery mode for disassembly view
+	 * @param memoryAt 1st location of unreadable memory
+	 * @param memoryLength length of unreadable memory (incoming null will "unknown" for length)
+	 * @return formatted message with location & length
+	 */
+	private static String cantReadMemory(String memoryAt, String memoryLength) {
+		memoryLength = (memoryLength == null) ? "unknown" : memoryLength;
+		return MessageFormat.format(EDCServicesMessages.Disassembly_CannotReadMemoryAt,
+									memoryAt, memoryLength);
+	}
+
+	/**
+	 * check each byte of incoming MemoryByte[] array to see if readable;
+	 * fills the RequestMonitor status in case of unreadable bytes.
+	 * @param memBytes data to translate
+	 * @param start address of first byte of memBytes
+	 * @param rm with which to set status in case of error
+	 * @return code buffer translated from incoming memByte array, null if any are unreadable
 	 * @since 2.0
 	 */
-	protected IStatus statusCannotReadMemory(String memoryAt) {
-		return new Status(IStatus.ERROR, EDCDebugger.PLUGIN_ID, REQUEST_FAILED,
-						  MessageFormat.format(EDCServicesMessages.Disassembly_CannotReadMemoryAt, memoryAt),
-						  null);
+	public static ByteBuffer translateMemoryBytes(MemoryByte[] memBytes,
+			IAddress start, RequestMonitor rm) {
+		byte[] bytes = new byte[memBytes.length];
+		for (int i = 0; i < memBytes.length; i++) {
+			// check each byte
+			if (!memBytes[i].isReadable()) {
+				rm.setStatus(statusCannotReadMemory(start.add(i).toHexAddressString(), 
+													((Integer)(memBytes.length-i))));
+				rm.done();
+				return null;
+			}
+			bytes[i] = memBytes[i].getValue();
+		}
+		return ByteBuffer.wrap(bytes);
+	}
+
+	/**
+	 * return a buffer of instruction code whose readability matches the
+	 * first byte of the data for as long as all such bytes are the same
+	 * @param memBytes data to translate
+	 * @return a code buffer either full of readable bytes
+	 * 		or empty representing the unreadable region
+	 */
+	private ByteBuffer translateMemoryBytes(List<MemoryByte> memBytes) {
+		byte[] bytes = new byte[memBytes.size()];
+		boolean firstIsReadable = memBytes.get(0).isReadable();
+		int count = 0;
+		for (MemoryByte memByte : memBytes) {
+			// check each byte
+			if (memByte.isReadable() != firstIsReadable)
+				break;
+			bytes[count++] = firstIsReadable ? memByte.getValue() : 0;
+		}
+
+		return ByteBuffer.wrap(bytes, 0, count);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.cdt.dsf.debug.service.IDisassembly#getInstructions(org.eclipse.cdt.dsf.debug.service.IDisassembly.IDisassemblyDMContext, java.math.BigInteger, java.math.BigInteger, org.eclipse.cdt.dsf.concurrent.DataRequestMonitor)
 	 */
-	public void getInstructions(IDisassemblyDMContext context, BigInteger startAddress, BigInteger endAddress,
+	public void getInstructions(final IDisassemblyDMContext context, BigInteger startAddress, BigInteger endAddress,
 			final DataRequestMonitor<IInstruction[]> drm) {
 
 		// FIXME: ignoring null startAddress and null endAddress semantics
 
-		final IDisassembler disassembler = getTargetEnvironmentService().getDisassembler();
+		ITargetEnvironment env = getTargetEnvironmentService();
+		final IDisassembler disassembler = (env != null) ? env.getDisassembler() : null;
 		if (disassembler == null) {
 			drm.setStatus(statusNoDisassembler());
 			drm.done();
@@ -106,42 +172,22 @@ public class Disassembly extends AbstractEDCService implements IDisassembly {
 
 		final IMemoryDMContext mem_dmc = DMContexts.getAncestorOfType(context, IMemoryDMContext.class);
 		final IAddress start = new Addr64(startAddress);
-		final IAddress end = new Addr64(endAddress);
 
 		memoryService.getMemory(mem_dmc, start, 0, 1, (int) size,
 								new DataRequestMonitor<MemoryByte[]>(getExecutor(), drm) {
 			@Override
 			protected void handleSuccess() {
-				MemoryByte[] memBytes = getData();
-				final byte[] bytes = new byte[memBytes.length];
-				for (int i = 0; i < memBytes.length; i++) {
-					// check each byte
-					if (!memBytes[i].isReadable()) {
-						drm.setStatus(statusCannotReadMemory(start.add(i).getValue().toString(16)));
-						drm.done();
-						return;
-					}
-					bytes[i] = memBytes[i].getValue();
-				}
-
-				ByteBuffer codeBuf = ByteBuffer.wrap(bytes);
-
+				List<MemoryByte> memBytes = Arrays.asList(getData());
 				Map<String, Object> options = new HashMap<String, Object>();
 				try {
-					List<IDisassembledInstruction> insts
-					  = disassembler.disassembleInstructions(start, end, codeBuf, options);
-
-					IInstruction[] ret = new IInstruction[insts.size()];
-					for (int i = 0; i < insts.size(); i++) {
-						ret[i] = new EDCInstruction(insts.get(i));
-					}
-
-					drm.setData(ret);
-					drm.done();
+					ArrayList<IInstruction> instrs
+					  = fillDisassemblyViewInstructions(memBytes, start, context,
+														disassembler, options);
+					drm.setData(instrs.toArray(new IInstruction[instrs.size()]));
 				} catch (CoreException e) {
 					drm.setStatus(e.getStatus());
-					drm.done();
 				}
+				drm.done();
 			}
 		});
 
@@ -179,7 +225,7 @@ public class Disassembly extends AbstractEDCService implements IDisassembly {
 	/* (non-Javadoc)
 	 * @see org.eclipse.cdt.dsf.debug.service.IDisassembly#getMixedInstructions(org.eclipse.cdt.dsf.debug.service.IDisassembly.IDisassemblyDMContext, java.math.BigInteger, java.math.BigInteger, org.eclipse.cdt.dsf.concurrent.DataRequestMonitor)
 	 */
-	public void getMixedInstructions(IDisassemblyDMContext context, BigInteger startAddress, BigInteger endAddress,
+	public void getMixedInstructions(final IDisassemblyDMContext context, BigInteger startAddress, BigInteger endAddress,
 			final DataRequestMonitor<IMixedInstruction[]> drm) {
 
 		// FIXME: ignoring null startAddress and null endAddress semantics
@@ -229,7 +275,7 @@ public class Disassembly extends AbstractEDCService implements IDisassembly {
 				protected void handleSuccess() {
 					List<IMixedInstruction> mixedInstructions = new ArrayList<IMixedInstruction>();
 
-					mixSource(mixedInstructions, module, codeLines, getData());
+					mixSource(mixedInstructions, null, module, codeLines, getData());
 
 					drm.setData(mixedInstructions.toArray(new IMixedInstruction[mixedInstructions.size()]));
 					drm.done();
@@ -246,7 +292,8 @@ public class Disassembly extends AbstractEDCService implements IDisassembly {
 
 		// FIXME: ignoring "lines" semantics
 
-		final IDisassembler disassembler = getTargetEnvironmentService().getDisassembler();
+		final ITargetEnvironment env = getTargetEnvironmentService();
+		final IDisassembler disassembler = (env != null) ? env.getDisassembler() : null;
 		if (disassembler == null) {
 			drm.setStatus(statusNoDisassembler());
 			drm.done();
@@ -272,39 +319,125 @@ public class Disassembly extends AbstractEDCService implements IDisassembly {
 		});
 	}
 
+	private EDCInstruction pseudoInstruction(IAddress address, int size, String pseudoMnemonic) {
+		DisassembledInstruction pseudoInstruction = new DisassembledInstruction();
+		pseudoInstruction.setAddress(address);
+		pseudoInstruction.setSize(size);
+		pseudoInstruction.setMnemonics(pseudoMnemonic);
+		return new EDCInstruction(pseudoInstruction);
+	}
+
+	/**
+	 * creates the array of instructions to be used to fill the disassembly view.
+	 * for a range containing any unreadable instructions, it will create a
+	 * fake instruction consisting of the address, the entire unreadable range,
+	 * and a message to fill in the mnemonics section about the unreadable range.
+	 * @param memBytes the buffer containing the bytes to be disassembled
+	 * @param start starting address corresponding to the buffer
+	 * @param context context for disassembly
+	 * @param disassembler the disassembler object to use
+	 * @param options to be passed to the disassembleInstructions() call
+	 * @return array of IInstruction
+	 * @throws CoreException can be thrown by disassembleInstructions().
+	 * @since 2.0
+	 */
+	protected ArrayList<IInstruction> fillDisassemblyViewInstructions(final List<MemoryByte> memBytes,
+			final IAddress start, final IDisassemblyDMContext context,
+			final IDisassembler disassembler, Map<String, Object> options)
+			throws CoreException {
+		ArrayList<IInstruction> ret = new ArrayList<IInstruction>();
+		for (int offset = 0, last = memBytes.size(); offset < last ;) {
+			ByteBuffer codeBuf = translateMemoryBytes(memBytes.subList(offset, last));
+			int codeBufSize = codeBuf.limit();
+			IAddress block = start.add(offset);
+			if (memBytes.get(offset).isReadable()) {
+				try {
+					List<IDisassembledInstruction> insts
+					  = disassembler.disassembleInstructions(block, block.add(codeBufSize),
+							  								 codeBuf, options, context);
+					if (insts.size() == 0)
+						break;
+					for (int i = 0; i < insts.size(); i++) {
+						ret.add(new EDCInstruction(insts.get(i)));
+						offset += insts.get(i).getSize();
+					}
+				} catch (CodeBufferUnderflowException e) {
+					if (offset == 0 && codeBufSize == last) {
+						// nothing in entire block can be disassembled;
+						// at least tell the Disassembly view code this much
+						ret.add(pseudoInstruction(start, codeBufSize,
+												  "Buffer Underflow during disassembly")); //$NON-NLS-1$
+					}
+					offset += codeBufSize;
+				}
+			} else {
+				ret.add(pseudoInstruction(block, codeBufSize,
+										  cantReadMemory(block.toHexAddressString(),
+			  					   						 ((Integer)codeBufSize).toString())));
+				offset += codeBufSize;
+			}
+		}
+		return ret;
+	}
+
 	/**
 	 * whereas the default implementation of getMixedInstructions() gets and
 	 * processes all the disassembly in one pass, some variants are known to need
 	 * to override the default implementation to break the retrieval into chunks.
+	 * <p>
 	 * this portion of the mixing is still the same within those chunks, though,
 	 * and so has been extracted and protected for use by extending variant classes.
 	 * @param mixedInstructions the growing list of instructions being processed
+	 * @param wholeFunctionName name for whole function; if null, will be calculated per line
 	 * @param module the module for this block of code
 	 * @param codeLines list of ILineEntry containing info for mixing
 	 * @param instructions the instructions to be mixed with the source
 	 * @since 2.0
 	 */
 	protected void mixSource(List<IMixedInstruction> mixedInstructions,
-			final IEDCModuleDMContext module, final List<ILineEntry> codeLines,
+			final EDCInstructionFunctionInfo wholeBlockInfo,
+			final IEDCModuleDMContext module,
+			final List<ILineEntry> codeLines,
 			final IInstruction[] instructions) {
 
 		List<IInstruction> instsForLine = new ArrayList<IInstruction>();
 
+		IPath filePath = null;
+		String osString = null;
+
+		EDCInstructionFunctionInfo functionInfo = wholeBlockInfo;
 		int k = 0, instsCnt = instructions.length, lineCnt = codeLines.size();
 		for (int i = 0; i < lineCnt && k < instsCnt; i++) {
 			// Now map the instructions to source lines to generate
 			// MixedInstructions.
-			//
 			instsForLine.clear();
 			ILineEntry line = codeLines.get(i);
 
-			while (k < instsCnt
-					&& module.toLinkAddress(new Addr64(instructions[k].getAdress()))
-											.compareTo(line.getHighAddress()) < 0) {
-				instsForLine.add(instructions[k]);
+			if (wholeBlockInfo == null && module != null) {
+				functionInfo = new EDCInstructionFunctionInfo(module, line);
+			}
+
+			while (k < instsCnt) {
+				EDCInstruction inst = (EDCInstruction)instructions[k];
+				IAddress linkAddress = module.toLinkAddress(new Addr64(inst.getAdress()));
+				if (linkAddress.compareTo(line.getHighAddress()) >= 0)
+					break;
+
+				if (functionInfo != null) {
+					inst.setFunctionName(functionInfo.getFunctionName());
+					IAddress functionBase = functionInfo.getFunctionStartAddress();
+					if (functionBase != null) {
+						inst.setOffset(functionBase.distanceTo(linkAddress).intValue());
+					}
+				}
+				instsForLine.add(inst);
 				k++;
 			}
-			mixedInstructions.add(new EDCMixedInstruction(line.getFilePath().toOSString(), line.getLineNumber(),
+			if (line.getFilePath() != filePath) {
+				filePath = line.getFilePath();
+				osString = (filePath != null) ? filePath.toOSString() : null;
+			}
+			mixedInstructions.add(new EDCMixedInstruction(osString, line.getLineNumber(),
 								  						  instsForLine.toArray(new IInstruction[instsForLine.size()])));
 		}
 	}
@@ -320,7 +453,7 @@ public class Disassembly extends AbstractEDCService implements IDisassembly {
 	 * @return the first address of an associated LineEntry if it can be found, else the initialStartAddress
 	 * @since 2.0
 	 */
-	protected IAddress getStartAddressForLineEntryContainingAddress(final IEDCSymbols symbolsService,
+	protected static IAddress getStartAddressForLineEntryContainingAddress(final IEDCSymbols symbolsService,
 			final IEDCModules modulesService, final ISymbolDMContext sym_dmc, final IAddress initialStartAddress,
 			final IAddress endAddress) {
 		assert symbolsService != null && modulesService != null;

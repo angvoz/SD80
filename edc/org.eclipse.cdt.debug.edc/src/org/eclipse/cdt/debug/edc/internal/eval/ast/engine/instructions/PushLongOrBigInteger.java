15e679b5d74084ee995f883644ecbf859be0140a
kryall <kryall>
2010-10-29 17:13:16 +0000

    Constants in expressions need to carry along type info

--- edc/org.eclipse.cdt.debug.edc/src/org/eclipse/cdt/debug/edc/internal/eval/ast/engine/instructions/PushLongOrBigInteger.java
+++ edc/org.eclipse.cdt.debug.edc/src/org/eclipse/cdt/debug/edc/internal/eval/ast/engine/instructions/PushLongOrBigInteger.java
@@ -29,6 +29,9 @@ public class PushLongOrBigInteger extends SimpleInstruction {
 	// if true, suffix had 'll' (but not 'l')
 	private boolean isLongLong;
 	
+	// if true, the original number was a hex or octal string
+	private boolean isHexOrOctal;
+	
 	// if not null, a big integer 
 	private BigInteger fBigInteger;
 	// otherwise, this is the value
@@ -74,6 +77,16 @@ public class PushLongOrBigInteger extends SimpleInstruction {
 		if (number == null)
 			number = fLong;
 		
+		// TODO: Handle 8-byte long and >8-byte long long
+
+		int intSize  = fInterpreter.getTypeEngine().getTypeSize(TypeUtils.BASIC_TYPE_INT);
+		int longSize = fInterpreter.getTypeEngine().getTypeSize(TypeUtils.BASIC_TYPE_LONG);
+		int longLongSize = fInterpreter.getTypeEngine().getTypeSize(TypeUtils.BASIC_TYPE_LONG_LONG);
+
+		// promote constant type based on the size of C/C++ int, long, and long long types.
+		// this cannot be done at parse time because the type engine is not known then
+		correctIntegerType(intSize * 8, longSize * 8, longLongSize * 8);
+
 		if (type == null) {
 			int flags = isLongLong ? ICPPBasicType.IS_LONG_LONG : isLong ?  ICPPBasicType.IS_LONG : 0;
 			
@@ -84,16 +97,143 @@ public class PushLongOrBigInteger extends SimpleInstruction {
 	
 			int size;
 			if (isLongLong)
-				size = fInterpreter.getTypeEngine().getTypeSize(TypeUtils.BASIC_TYPE_LONG_LONG);
+				size = longLongSize;
 			else if (isLong)
-				size = fInterpreter.getTypeEngine().getTypeSize(TypeUtils.BASIC_TYPE_LONG);
+				size = longSize;
 			else
-				size = fInterpreter.getTypeEngine().getTypeSize(TypeUtils.BASIC_TYPE_INT);
+				size = intSize;
 			
 			type = fInterpreter.getTypeEngine().getBasicType(ICPPBasicType.t_int, flags, size);
 		}
 		
 		pushNewValue(type, number);
+	}
+
+	/**
+	 *	Fix type of constant based on C++ promotion rules, which depend on the size of
+	 *  int, long, and long long types.
+	 *
+	 * @param intBitSize
+	 * @param longBitSize
+	 * @param longLongBitSize
+	 */
+	private void correctIntegerType(int intBitSize, int longBitSize, int longLongBitSize) {
+		if (    fBigInteger == null
+				// check if it fits in signed long (but maybe not an unsigned int)
+			&& (   (fLong >= 0 && (fLong >> (intBitSize - 1)) == 0)
+				|| (fLong <  0 && (fLong >> (intBitSize - 1)) < -1)))
+				return;
+
+		// at execute() time, isUnsigned means explicit unsigned, isLong means explicit long,
+		// and isLongLong means explicit long long
+		if (fBigInteger == null) {
+			// it fits in a 64-bit signed Java long
+			if (isUnsigned) {
+				// explicit unsigned decimal, hex, or octal constant
+				if (isLong) {
+					// explicit unsigned long may change to unsigned long long
+					if ((fLong >> longBitSize) > 0) {
+						isLong = false;
+						isLongLong = true;
+					}
+				} else if (!isLongLong) {
+					// not declared long long - may be int, long, or long long
+					if ((fLong >> (intBitSize - 1)) <= 1) {
+						// fits in an unsigned int
+					} else if ((fLong >> (longBitSize - 1)) <= 1) {
+						// fits in an unsigned long
+						isLong = true;
+					} else {
+						// fits in an unsigned longLong
+						isLongLong = true;
+					}
+				}
+			} else {
+				// signed decimal, hex, or octal constant
+				if (!isHexOrOctal) {
+					// decimal constant
+					if (!isLong && !isLongLong){
+						// signed decimal constant, not declared long or long long
+						if (   (fLong >= 0 && (fLong >> (longBitSize - 1)) == 0)
+							|| (fLong <  0 && (fLong >> (longBitSize - 1)) < -1)) {
+							// fits in signed long
+							isLong = true;
+						} else {
+							// fits in signed long long
+							isLongLong = true;
+						}
+					} else if (isLong) {
+						// explicit long may change to long long
+						if (!(   (fLong >= 0 && (fLong >> (longLongBitSize - 1)) == 0)
+							  || (fLong <  0 && (fLong >> (longLongBitSize - 1)) < -1))) {
+							isLong = false;
+							isLongLong = true;
+						}
+					}
+				} else {
+					// hex or octal constant
+					if (!isLong && !isLongLong){
+						// signed hex or octal constant, not declared long or long long
+						// may be int, unsigned int, long, unsigned long, long long, or unsigned long long
+						if (fLong >> (intBitSize - 1) == 1) {
+							// fits in unsigned int
+							isUnsigned = true;
+						} else if (   (fLong >= 0 && (fLong >> (longBitSize - 1)) == 0)
+								   || (fLong <  0 && (fLong >> (longBitSize - 1)) < -1)) {
+							// fits in signed long
+							isLong = true;
+						} else if (fLong >> (longBitSize - 1) == 1) {
+							// fits in unsigned long
+							isLong = true;
+							isUnsigned = true; 
+						} else if (   (fLong >= 0 && (fLong >> (longLongBitSize - 1)) == 0)
+								   || (fLong <  0 && (fLong >> (longLongBitSize - 1)) < -1)) {
+							// fits in signed long long
+							isLongLong = true;
+						} else {
+							// fits in unsigned long long
+							isLongLong = true;
+							isUnsigned = true;
+						}
+					} else if (isLong) {
+						// explicit long hex or octal may change to unsigned long, long long, or unsigned long long
+						if (   (fLong >= 0 && (fLong >> (longBitSize - 1)) == 0)
+							|| (fLong <  0 && (fLong >> (longBitSize - 1)) < -1)) {
+							// fits in signed long
+						} else if (fLong >> (longBitSize - 1) == 1) {
+							// fits in unsigned long
+							isUnsigned = true; 
+						} else if (   (fLong >= 0 && (fLong >> (longLongBitSize - 1)) == 0)
+								   || (fLong <  0 && (fLong >> (longLongBitSize - 1)) < -1)) {
+							// fits in signed long long
+							isLong = false;
+							isLongLong = true;
+						} else {
+							// fits in unsigned long long
+							isLong = false;
+							isLongLong = true;
+							isUnsigned = true;
+						}
+					} else {
+						// explicit long long may change to unsigned long long
+						if (fLong >> (longLongBitSize - 1) > 0)
+							isUnsigned = true;
+					}
+				}
+			}
+		} else {
+			// BigInteger, which does not fit in a Java long
+			isLongLong = true;
+			isLong = false;
+			if (!isUnsigned) {
+				// signed decimal, hex, or octal long long constant
+				if (isHexOrOctal) {
+					// for hex or octal constant, long long may change to unsigned long long
+					if (fBigInteger.bitLength() > (longLongBitSize - 1))
+						isUnsigned = true;
+				}
+			}
+		}
 	}
 
 	/**
@@ -118,6 +258,7 @@ public class PushLongOrBigInteger extends SimpleInstruction {
 	 */
 	public void parseLongOrBigInteger(String value) throws NumberFormatException {
 		isUnsigned = false;
+		isHexOrOctal = false;
 
 		String val = value.toLowerCase();
 		int length = value.length();
@@ -152,6 +293,7 @@ public class PushLongOrBigInteger extends SimpleInstruction {
 		// if after BigInteger conversion, Long conversion fails, the value is
 		// too large or small
 		if (val.startsWith("0x")) { //$NON-NLS-1$
+			isHexOrOctal = true;
 			try {
 				fLong = Long.valueOf(val.substring(2), 16);
 			} catch (NumberFormatException nfe) {
@@ -161,6 +303,7 @@ public class PushLongOrBigInteger extends SimpleInstruction {
 			}
 			length -= 2;
 		} else if (length > 1 && val.startsWith("0")) { //$NON-NLS-1$
+			isHexOrOctal = true;
 
 			try {
 				fLong = Long.valueOf(val.substring(1), 8);

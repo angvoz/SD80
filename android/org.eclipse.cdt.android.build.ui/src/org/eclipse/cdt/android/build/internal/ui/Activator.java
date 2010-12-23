402939d5a213f256f069b6cb8c6e93d1f11e1632
dschaefer <dschaefer>
2010-12-23 06:32:09 +0000

    A few more updates to get everything working.

--- android/org.eclipse.cdt.android.build.ui/src/org/eclipse/cdt/android/build/internal/ui/Activator.java
+++ android/org.eclipse.cdt.android.build.ui/src/org/eclipse/cdt/android/build/internal/ui/Activator.java
@@ -20,29 +20,16 @@ public class Activator extends AbstractUIPlugin {
 	public Activator() {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
 	public static Activator getDefault() {
 		return plugin;
 	}
--- android/org.eclipse.cdt.android.build.ui/src/org/eclipse/cdt/android/build/internal/ui/AddNativeAction.java
+++ android/org.eclipse.cdt.android.build.ui/src/org/eclipse/cdt/android/build/internal/ui/AddNativeAction.java
@@ -1,11 +1,19 @@
 package org.eclipse.cdt.android.build.internal.ui;
+import java.io.IOException;
+
+import org.eclipse.cdt.android.build.core.NDKManager;
 import org.eclipse.cdt.core.CCorePlugin;
 import org.eclipse.cdt.make.core.MakeCorePlugin;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.PlatformObject;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -31,18 +39,32 @@ public class AddNativeAction implements IObjectActionDelegate {
 				}
 			}
 		}
-		if (project != null)
+		if (project != null) {
+			final IProject proj = project;
+			IWorkspaceRunnable op = new IWorkspaceRunnable() {
+				@Override
+				public void run(IProgressMonitor monitor) throws CoreException {
+					// Convert to CDT project
+					CCorePlugin.getDefault().convertProjectToCC(proj, monitor, MakeCorePlugin.MAKE_PROJECT_ID);
+					// Set up build information
+					new NDKWizardHandler().convertProject(proj, monitor);
+					// Set up the source and output folders
+					try {
+						NDKManager.addNativeSupport(proj, "jni", monitor);
+					} catch (IOException e) {
+						throw new CoreException(new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e));
+					}
+				}
+			};
+
+			// TODO run from a job
+			IWorkspace workspace = ResourcesPlugin.getWorkspace();
 			try {
-				// TODO see NewCProjectWizard.getRunnable() on how to set this up.
-				IProgressMonitor monitor = new NullProgressMonitor();
-				// Convert to CDT project
-				CCorePlugin.getDefault().convertProjectToCC(project, monitor, MakeCorePlugin.MAKE_PROJECT_ID);
-				// Set up build information
-				new NDKWizardHandler().convertProject(project, monitor);
+				workspace.run(op, workspace.getRoot(), 0, new NullProgressMonitor());
 			} catch (CoreException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
+				Activator.getDefault().getLog().log(e.getStatus());
+			}
+		}
 	}
 
 	@Override

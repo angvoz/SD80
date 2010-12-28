c6649440acad6e31c7f5ef702e4da001c86664d4
dschaefer <dschaefer>
2010-12-28 07:02:52 +0000

    Introducing a template to handle the project setup.

--- android/org.eclipse.cdt.android.build.core/src/org/eclipse/cdt/android/build/core/NDKManager.java
+++ android/org.eclipse.cdt.android.build.core/src/org/eclipse/cdt/android/build/core/NDKManager.java
@@ -5,18 +5,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.cdt.android.build.internal.core.Activator;
-import org.eclipse.cdt.core.CCorePlugin;
-import org.eclipse.cdt.core.model.CoreModel;
-import org.eclipse.cdt.core.model.ICProject;
-import org.eclipse.cdt.core.model.IPathEntry;
+import org.eclipse.cdt.core.templateengine.TemplateCore;
+import org.eclipse.cdt.core.templateengine.TemplateEngine;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
@@ -57,48 +52,18 @@ public class NDKManager {
 	public static void addNativeSupport(final IProject project, final String libraryName, IProgressMonitor monitor) 
 			throws CoreException, IOException {
 
-		// TODO make this a real CDT template so we can call from the new project wizard for
-		// Java free native projects.
+		// Launch our template to set up the project contents
+		TemplateCore template = TemplateEngine.getDefault().getTemplateById("AddNDKSupport");
+		Map<String, String> valueStore = template.getValueStore();
+		valueStore.put("projectName", project.getName());
+		template.executeTemplateProcesses(monitor, false);
 		
-		// Create the source and output folders
-		IFolder sourceFolder = project.getFolder("jni");
-		if (!sourceFolder.exists())
-			sourceFolder.create(true, true, monitor);
-		IPathEntry sourceEntry = CoreModel.newSourceEntry(sourceFolder.getFullPath());
-				
-		IFolder libFolder = project.getFolder("libs");
-		if (!libFolder.exists())
-			libFolder.create(true, true, monitor);
-		IPathEntry libEntry = CoreModel.newOutputEntry(libFolder.getFullPath());
-		
-		IFolder objFolder = project.getFolder("obj");
-		if (!objFolder.exists())
-			objFolder.create(true, true, monitor);
-		IPathEntry objEntry = CoreModel.newOutputEntry(objFolder.getFullPath());
-		
-		// Set up the path entries for the source and output folders
-		CoreModel model = CCorePlugin.getDefault().getCoreModel();
-		ICProject cproject = model.create(project);
-		IPathEntry[] pathEntries = cproject.getRawPathEntries();
-		List<IPathEntry> newEntries = new ArrayList<IPathEntry>(pathEntries.length + 2);
-		for (IPathEntry pathEntry : pathEntries) {
-			// remove the old source and output entries
-			if (pathEntry.getEntryKind() != IPathEntry.CDT_SOURCE
-					&& pathEntry.getEntryKind() != IPathEntry.CDT_OUTPUT) {
-				newEntries.add(pathEntry);
-			}
-		}
-		newEntries.add(sourceEntry);
-		newEntries.add(libEntry);
-		newEntries.add(objEntry);
-		cproject.setRawPathEntries(newEntries.toArray(new IPathEntry[newEntries.size()]), monitor);
-				
 		// Generate the Android.mk and initial source file
-		// TODO make this a real CDT template
+		// TODO add this to the template
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("lib", libraryName);
 
-		IFile makefile = sourceFolder.getFile("Android.mk");
+		IFile makefile = project.getFile(new Path("jni/Android.mk"));
 		if (!makefile.exists()) {
 			URL mkURL = Activator.find(new Path("templates/Android.mk"));
 			String contents = readFile(mkURL);
@@ -106,10 +71,10 @@ public class NDKManager {
 			
 			InputStream contentsIn = new ByteArrayInputStream(contents.getBytes());
 			makefile.create(contentsIn, true, monitor);
-				
+
 			// Copy over initial source file
 			// TODO we should allow C or C++ files
-			IFile srcFile = sourceFolder.getFile(libraryName + ".cpp");
+			IFile srcFile = project.getFile(new Path("jni/" + libraryName + ".cpp"));
 			if (!srcFile.exists()) {
 				URL srcURL = Activator.find(new Path("templates/jni.cpp"));
 				InputStream srcIn = srcURL.openStream();

7b820d5b8bcec72b744963838904d201679157e4
dschaefer <dschaefer>
2011-01-04 04:56:41 +0000

    Making it official with copyright and other legal notices.

--- android/org.eclipse.cdt.android.build.ui/src/org/eclipse/cdt/android/build/internal/ui/AddNativeAction.java
+++ android/org.eclipse.cdt.android.build.ui/src/org/eclipse/cdt/android/build/internal/ui/AddNativeAction.java
@@ -1,4 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 Wind River Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Wind River Systems - Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.cdt.android.build.internal.ui;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.PlatformObject;
 import org.eclipse.jface.action.IAction;
@@ -7,7 +18,6 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
-
 
 public class AddNativeAction implements IObjectActionDelegate {
 

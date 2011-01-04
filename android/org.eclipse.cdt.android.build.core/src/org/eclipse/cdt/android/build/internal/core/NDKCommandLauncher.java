d6c19084bbd05ad4767757f0f305b0e8823b70e8
dschaefer <dschaefer>
2011-01-04 04:56:44 +0000

    Making it official with copyright and other legal notices.

--- android/org.eclipse.cdt.android.build.core/src/org/eclipse/cdt/android/build/internal/core/NDKCommandLauncher.java
+++ android/org.eclipse.cdt.android.build.core/src/org/eclipse/cdt/android/build/internal/core/NDKCommandLauncher.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 Wind River Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Wind River Systems - Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.cdt.android.build.internal.core;
 
 import org.eclipse.cdt.core.CommandLauncher;

7c4e0133318968037d07cba8605cb5d72d02d7ee
dschaefer <dschaefer>
2011-01-27 05:04:34 +0000

    Add NDK location setting to the wizard page. Also automatically switch to C/C++ perspective on conversion.

--- android/org.eclipse.cdt.android.build.ui/src/org/eclipse/cdt/android/build/internal/ui/AddNativeWizardPage.java
+++ android/org.eclipse.cdt.android.build.ui/src/org/eclipse/cdt/android/build/internal/ui/AddNativeWizardPage.java
@@ -15,35 +15,75 @@ import java.util.Map;
 import org.eclipse.cdt.android.build.core.NDKManager;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 public class AddNativeWizardPage extends WizardPage {
 	
 	private final String defaultLibraryName;
+	private final String currentNDKLocation;
 	private Text libraryNameText;
+	private Text ndkLocationText;
 
 	public AddNativeWizardPage(Map<String, String> templateArgs) {
 		super("addNativeWizardPage");
-		defaultLibraryName = templateArgs.get(NDKManager.LIBRARY_NAME);
 		setDescription("Settings for generated native components for project.");
 		setTitle("Add Android Native Support");
+
+		defaultLibraryName = templateArgs.get(NDKManager.LIBRARY_NAME);
+		currentNDKLocation = NDKManager.getNDKLocation();
 	}
 	
 	@Override
 	public void createControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NULL);
 		setControl(container);
-		container.setLayout(new GridLayout(2, false));
+		container.setLayout(new GridLayout(1, false));
 		
-		Label lblLibraryName = new Label(container, SWT.NONE);
-		lblLibraryName.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		Group grpWorkspaceSettings = new Group(container, SWT.NONE);
+		grpWorkspaceSettings.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		grpWorkspaceSettings.setText("Workspace Settings");
+		grpWorkspaceSettings.setLayout(new GridLayout(2, false));
+		
+		Label lblNdkLocation = new Label(grpWorkspaceSettings, SWT.NONE);
+		lblNdkLocation.setText("NDK Location:");
+		
+		Composite composite_1 = new Composite(grpWorkspaceSettings, SWT.NONE);
+		composite_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		composite_1.setLayout(new GridLayout(2, false));
+		
+		ndkLocationText = new Text(composite_1, SWT.BORDER);
+		ndkLocationText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		ndkLocationText.setText(currentNDKLocation);
+		
+		Button btnBrowse = new Button(composite_1, SWT.NONE);
+		btnBrowse.setText("Browse...");
+		btnBrowse.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				String dir = new DirectoryDialog(getShell()).open();
+				if (dir != null)
+					ndkLocationText.setText(dir);
+			}
+		});
+		
+		Group grpProjectSettings = new Group(container, SWT.NONE);
+		grpProjectSettings.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		grpProjectSettings.setText("Project Settings");
+		grpProjectSettings.setLayout(new GridLayout(2, false));
+		
+		Label lblLibraryName = new Label(grpProjectSettings, SWT.NONE);
 		lblLibraryName.setText("Library Name:");
 		
-		Composite composite = new Composite(container, SWT.NONE);
+		Composite composite = new Composite(grpProjectSettings, SWT.NONE);
 		composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		composite.setLayout(new GridLayout(3, false));
 		
@@ -60,5 +100,9 @@ public class AddNativeWizardPage extends WizardPage {
 
 	public void updateArgs(Map<String, String> templateArgs) {
 		templateArgs.put(NDKManager.LIBRARY_NAME, libraryNameText.getText());
+		
+		String newNDKLocation = ndkLocationText.getText();
+		if (!newNDKLocation.equals(currentNDKLocation))
+			NDKManager.setNDKLocation(newNDKLocation);
 	}
 }
